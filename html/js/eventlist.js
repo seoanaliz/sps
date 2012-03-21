9aa4ab25a0eea4c8e2b4bce46e6f1debf8c870b9
shuler <shuler@bff4bcb2-3a6d-e111-ad23-00104b9ce278>
2012-03-21 06:12:45 +0000

    [+] html

---
+++ html/js/eventlist.js
@@ -0,0 +1,9 @@
+var Eventlist = {
+    leftcolumn_deletepost: function(post_id){},
+    rightcolumn_deletepost: function(post_id){},
+    leftcolumn_dropdown_change: function(sel){},
+    rightcolumn_dropdown_change: function(sel){},
+    calendar_change: function(timestamp){alert(timestamp)},
+    /*post_moved: function(post_id, from, to){}*/
+    eof: null
+}
