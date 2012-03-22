0a09b477580ed891de15ad0385dca4918d06c7a1
shuler <shuler@bff4bcb2-3a6d-e111-ad23-00104b9ce278>
2012-03-22 11:59:17 +0000

    [~] test.sql [~] templates error bugfix

--- web/etc/templates/fe/elements/arcticles-list.tmpl.php
+++ web/etc/templates/fe/elements/arcticles-list.tmpl.php
@@ -2,28 +2,30 @@
     /** @var $articles Article[] */
     /** @var $articleRecords articleRecord[] */
     /** @var $sourceFeed SourceFeed */
-    foreach($articles as $article) {
-        $articleRecord = !empty($articleRecords[$article->articleId]) ? $articleRecords[$article->articleId] : new ArticleRecord();
-        ?>
-            <div class="post bb" data-id="{$article->articleId}">
-                <div class="l d-hide">
-                    <div class="userpic"><img/></div>
-                </div>
-                <div class="name d-hide">{$sourceFeed->title}</div>
-                <div class="content">
-                    <?= nl2br($articleRecord->content) ?>
-                    <? foreach($articleRecord->photos as $photoItem) { ?>
-                        <br /><img src="<?= MediaUtility::GetFilePath( 'Article', 'photos', 'small', $photoItem['filename'], MediaServerManager::$MainLocation) ?>">
-                    <? } ?>
-                </div>
-                <div class="bottom d-hide">
-                    <div class="l"><span class="timestamp">{$article->createdAt->defaultFormat()}</span> | <a href="javascript:;">Редактировать</a></div>
-                    <div class="r"><span class="original"><a href="http://vk.com/wall-{$article->externalId}" target="_blank">Оригинал</a></span> | <span class="likes spr"></span><span class="likes-count">{$articleRecord->likes}</span></div>
-                </div>
-                <div class="delete spr"></div>
-                <div class="clear"></div>
+    if (!empty($articles)) {
+        foreach($articles as $article) {
+            $articleRecord = !empty($articleRecords[$article->articleId]) ? $articleRecords[$article->articleId] : new ArticleRecord();
+            ?>
+        <div class="post bb" data-id="{$article->articleId}">
+            <div class="l d-hide">
+                <div class="userpic"><img/></div>
             </div>
+            <div class="name d-hide">{$sourceFeed->title}</div>
+            <div class="content">
+                <?= nl2br($articleRecord->content) ?>
+                <? foreach($articleRecord->photos as $photoItem) { ?>
+                <br /><img src="<?= MediaUtility::GetFilePath( 'Article', 'photos', 'small', $photoItem['filename'], MediaServerManager::$MainLocation) ?>">
+                <? } ?>
+            </div>
+            <div class="bottom d-hide">
+                <div class="l"><span class="timestamp">{$article->createdAt->defaultFormat()}</span> | <a href="javascript:;">Редактировать</a></div>
+                <div class="r"><span class="original"><a href="http://vk.com/wall-{$article->externalId}" target="_blank">Оригинал</a></span> | <span class="likes spr"></span><span class="likes-count">{$articleRecord->likes}</span></div>
+            </div>
+            <div class="delete spr"></div>
+            <div class="clear"></div>
+        </div>
         <?
+        }
     }
 ?>
 <script type="text/javascript">
