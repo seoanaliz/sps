0f613f1f03133de5c54424c59f8aaa5747bdfc4d
shuler <shuler@bff4bcb2-3a6d-e111-ad23-00104b9ce278>
2012-03-22 11:54:09 +0000

    [~] temp.sql

--- docs/test.sql
+++ docs/test.sql
@@ -5,4 +5,8 @@ INSERT INTO "public"."publishers" ("name", "vk_id", "vk_app", "vk_token", "vk_se
 VALUES ('test publisher', 0, 0, 'c7ac1842c3ddb889c3ddb88974c3f6e60dcc3ddc3d8801f85c16a3c7336abd4', 'V1us1w3lbkoaapuYiddg', 1);
 
 INSERT INTO "public"."targetFeeds" ("title", "externalId", "publisherId", "statusId")
-VALUES ('test wall', '27421965', 1, 1);
+VALUES ('test wall 1', '27421965', 1, 1);
+INSERT INTO "public"."targetFeeds" ("title", "externalId", "publisherId", "statusId")
+VALUES ('test wall 2', '27421965', 1, 1);
+INSERT INTO "public"."targetFeeds" ("title", "externalId", "publisherId", "statusId")
+VALUES ('test wall 3', '27421965', 1, 1);
