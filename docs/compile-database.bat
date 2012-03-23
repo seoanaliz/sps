4b163f627f08d1f2a2ae9b1bcfa5e14fcef9755d
shuler <shuler@bff4bcb2-3a6d-e111-ad23-00104b9ce278>
2012-03-23 10:11:36 +0000

    [~] removed test.sql from compile

--- docs/compile-database.bat
+++ docs/compile-database.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET FILE="d:\www\SPS\docs\init-dump.sql"
 If Not Exist "%FILE%" Echo.>"%FILE%"
-copy /b SPS.sql+Views\*.*+init.sql+test.sql %FILE%
+copy /b SPS.sql+Views\*.*+init.sql %FILE%
 pause
--- docs/init-dump.sql
+++ docs/init-dump.sql
@@ -597,17 +597,4 @@ INSERT INTO "vfsFoldersTree" 							VALUES ( 1, NULL, '1', NULL, NULL );
 INSERT INTO "public"."auditEventTypes" ("title", "alias")
 VALUES ('Ошибки импорта', 'importErrors');
 INSERT INTO "public"."auditEventTypes" ("title", "alias")
-VALUES ('Ошибки экспорта', 'exportErrors');INSERT INTO "public"."sourceFeeds" ("title", "externalId", "statusId")
-VALUES ('animal.world', '27052479', 1);
-INSERT INTO "public"."sourceFeeds" ("title", "externalId", "statusId")
-VALUES ('Лучшие автомобили | ВКурсе', '35807044', 1);
-
-INSERT INTO "public"."publishers" ("name", "vk_id", "vk_app", "vk_token", "vk_seckey", "statusId")
-VALUES ('test publisher', 0, 0, 'c7ac1842c3ddb889c3ddb88974c3f6e60dcc3ddc3d8801f85c16a3c7336abd4', 'V1us1w3lbkoaapuYiddg', 1);
-
-INSERT INTO "public"."targetFeeds" ("title", "externalId", "publisherId", "statusId")
-VALUES ('test wall 1', '27421965', 1, 1);
-INSERT INTO "public"."targetFeeds" ("title", "externalId", "publisherId", "statusId")
-VALUES ('test wall 2', '27421965', 1, 1);
-INSERT INTO "public"."targetFeeds" ("title", "externalId", "publisherId", "statusId")
-VALUES ('test wall 3', '27421965', 1, 1);
+VALUES ('Ошибки экспорта', 'exportErrors');
