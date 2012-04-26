fcc272dfa944ea68b41d4580ad25a47d9a024fc2
Eugene Shulepin <eugene.shulepin@gmail.com>
2012-04-26 17:45:08 +0400

    фикс получения параметров ссылки

--- web/lib/SPS.Site/actions/controls/GetLinkInfoControl.php
+++ web/lib/SPS.Site/actions/controls/GetLinkInfoControl.php
@@ -15,7 +15,7 @@
         public function Execute() {
             $result = array();
 
-            $url =Request::getString('url');
+            $url = Request::getString('url');
             if ($url) {
                 $metaDetail = MetaDetailFactory::GetOne(array('url' => trim($url, '/')));
                 if (!empty($metaDetail)) {
@@ -25,7 +25,7 @@
                     $result['img'] = !empty($metaDetail->alt) ? $metaDetail->alt : '';
 
                     if (!empty($metaDetail->alt)) {
-                        $result['img'] = MediaUtility::GetFilePath( 'Link', 'photos', 'original', $metaDetail->alt, MediaServerManager::$MainLocation );;
+                        $result['img'] = MediaUtility::GetFilePath( 'Link', 'photos', 'small', $metaDetail->alt, MediaServerManager::$MainLocation );;
                     } else {
                         $result['img'] = '';
                     }
