a869d537409b8656db52ee1187b9bdebcd3db3bf
akalie <askaslie@gmail.com>
2012-05-03 17:56:45 +0400

    Update docs/api-classes/vk_poster.php

--- docs/api-classes/vk_poster.php
+++ docs/api-classes/vk_poster.php
@@ -1,79 +1,79 @@
-<?php
-header("Content-Type: text/html; charset=utf-8");
-class Vsend{
-
-    protected $post_photo_array;    //массив адресов фоток
-    protected $post_text;           //текст поста
-    protected $attachments = '';    //аттачи
-    protected $vk_access_token;
-    protected $vk_group_id;         //id паблика, куда постим
-    protected $vk_aplication_id;    //id аппа, с которого постим
-    protected $vk_app_seckey;       //
-    protected $link;                //ссылка на источник
-    protected $sign;                //ссыль на пользователя, пока неактивно
-    protected $header;              //заголовок ссылки
-    const METH = 'https://api.vk.com/method/';
+    <?php
+    header("Content-Type: text/html; charset=utf-8");
+    class Vsend{
     
-    public function __construct($post_date)
-    {
-        $post_date = json_decode($post_date);
-        print_r($post_date);
-        $this->post_photo_array     = $post_date -> photo_array; //массив путей к фоткам
-        $this->post_text            = $post_date -> text;
-        $this->vk_group_id          = $post_date -> group_id;
-        $this->vk_app_seckey        = $post_date -> vk_app_seckey;
-        $this->vk_access_token      = $post_date -> vk_access_token;
-        $this->audio_id             = $post_date -> audio_id;//массив вида array('videoXXXX_YYYYYYY','...')
-        $this->video_id             = $post_date -> video_id;//массив вида array('audioXXXX_YYYYYYY','...')
-        $this->link                 = $post_date -> link;
-        $this->header               = $post_date -> header; 
-    }
-    
-    private function qurl_request($url, $arr_of_fields, $headers = '', $uagent = '')
-    {
-        if (empty($url)) {
-            return false; 
+        protected $post_photo_array;    //массив адресов фоток
+        protected $post_text;           //текст поста
+        protected $attachments = '';    //аттачи
+        protected $vk_access_token;
+        protected $vk_group_id;         //id паблика, куда постим
+        protected $vk_aplication_id;    //id аппа, с которого постим
+        protected $vk_app_seckey;       //
+        protected $link;                //ссылка на источник
+        protected $sign;                //ссыль на пользователя, пока неактивно
+        protected $header;              //заголовок ссылки
+        const METH = 'https://api.vk.com/method/';
+        
+        public function __construct($post_date)
+        {
+            $post_date = json_decode($post_date);
+            print_r($post_date);
+            $this->post_photo_array     = $post_date -> photo_array; //массив путей к фоткам
+            $this->post_text            = $post_date -> text;
+            $this->vk_group_id          = $post_date -> group_id;
+            $this->vk_app_seckey        = $post_date -> vk_app_seckey;
+            $this->vk_access_token      = $post_date -> vk_access_token;
+            $this->audio_id             = $post_date -> audio_id;//массив вида array('videoXXXX_YYYYYYY','...')
+            $this->video_id             = $post_date -> video_id;//массив вида array('audioXXXX_YYYYYYY','...')
+            $this->link                 = $post_date -> link;
+            $this->header               = $post_date -> header; 
         }
         
-        $ch = curl_init($url);
-        print_r($arr_of_fields);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-
-        if (is_array($headers)) { // если заданы какие-то заголовки для браузера 
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-        } 
-
-        if (!empty($uagent)) { // если задан UserAgent 
-            curl_setopt($ch, CURLOPT_USERAGENT, $uagent); 
-        } else{
-            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1)');
-        }
-
-        curl_setopt($ch, CURLOPT_POST, 1); 
-        curl_setopt($ch, CURLOPT_HEADER, 0); 
-        if (is_array($arr_of_fields)) { 
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $arr_of_fields); 
+        private function qurl_request($url, $arr_of_fields, $headers = '', $uagent = '')
+        {
+            if (empty($url)) {
+                return false; 
+            }
             
-        } else return false;
-
-        $result = curl_exec($ch); 
-        if (curl_errno($ch)){
-            echo "<br>error in curl: ". curl_error($ch) ."<br>";
-            throw new Exception('error in curl: '. curl_error($ch)) ;
-        }
-  
-        curl_close($ch); 
-        return $result; 
-    }
+            $ch = curl_init($url);
+            print_r($arr_of_fields);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    //        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
     
-    public function send_post()
+            if (is_array($headers)) { // если заданы какие-то заголовки для браузера 
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
+            } 
+    
+            if (!empty($uagent)) { // если задан UserAgent 
+                curl_setopt($ch, CURLOPT_USERAGENT, $uagent); 
+            } else{
+                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1)');
+            }
+    
+            curl_setopt($ch, CURLOPT_POST, 1); 
+            curl_setopt($ch, CURLOPT_HEADER, 0); 
+            if (is_array($arr_of_fields)) { 
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $arr_of_fields); 
+                
+            } else return false;
+    
+            $result = curl_exec($ch); 
+            if (curl_errno($ch)){
+                echo "<br>error in curl: ". curl_error($ch) ."<br>";
+                throw new Exception('error in curl: '. curl_error($ch)) ;
+            }
+      
+            curl_close($ch); 
+            return $result; 
+        }
+        
+         public function send_post()
     {
         $attachment = array(); 
-        
+        print_r($this->vk_access_token);
         $fields1 = array(    'gid'           =>  $this->vk_group_id,
                              'access_token'  =>  $this->vk_access_token);
 //        if (is_array($this->post_photo_array)){
@@ -142,15 +142,13 @@ class Vsend{
         if (!empty($this->video_id)){
             $attachment .= ',' . implode(',', $this->video_id);
         }
+        if (is_array($this->post_photo_array)) echo 'count = ' .  count($this->post_photo_array) . '<br>';
 
-        if($this->post_text == '' && count($this->post_photo_array) == 0){
-           $this->post_text = $this->header;
+        if (($this->post_text == '©' || $this->post_text == '') && !is_array($this->post_photo_array)){
+            $this->post_text =  "&#01;";
+            echo 'asdasd';
         }
-
-        if (($this->post_text =='©' || $this->post_text == '' ) && count($this->post_photo_array) == 0) {
-            $this->post_text = "&#01;";
-        }
-
+        
         $arr_fields = array('owner_id'      =>  '-'.$this->vk_group_id,
                             'message'       =>  $this->post_text,
                             'access_token'  =>  $this->vk_access_token,
@@ -214,44 +212,44 @@ class Vsend{
         }
    }
    
-    
-    private function remove_tags()
-    {
-         $this->post_text = str_replace( '<br>', "\r\n", $this->post_text );
-         $this->post_text = htmlspecialchars_decode($this->post_text);
-         $this->post_text = strip_tags( $this->post_text );
+        
+        private function remove_tags()
+        {
+             $this->post_text = str_replace( '<br>', "\r\n", $this->post_text );
+             $this->post_text = htmlspecialchars_decode($this->post_text);
+             $this->post_text = strip_tags( $this->post_text );
+        }
+        
+        //!!!распознование капчи - долгий и неблагодарный процесс(до полуминуты,
+        // + он может вернуть нераспознанную)
+        // нужно учитывать это время
+        // 
+        //возвращает массив с номером капчи и ее распознанностью, 
+        // false в случае неправильной разгадки/недоступности работников распознавания
+        //исключение в остальных случаях
+        private function get_captcha()
+        {
+            
+            
+        }
     }
     
-    //!!!распознование капчи - долгий и неблагодарный процесс(до полуминуты,
-    // + он может вернуть нераспознанную)
-    // нужно учитывать это время
-    // 
-    //возвращает массив с номером капчи и ее распознанностью, 
-    // false в случае неправильной разгадки/недоступности работников распознавания
-    //исключение в остальных случаях
-    private function get_captcha()
-    {
-        
-        
+    //данные тестового паблика
+    //$vk_group_id = 27421965;
+    //$vk_aplication_id = 2842244; //id аппа
+    //$vk_access_token = 'c7ac1842c3ddb889c3ddb88974c3f6e60dcc3ddc3d8801f85c16a3c7336abd4'; //
+    //$vk_app_seckey = V1us1w3lbkoaapuYiddg;
+    
+    //заголовок - поле header
+    try{
+        echo 'start<br>';
+        $a = new Vsend($post_array);
+        echo $a->send_post();
+    
+     
+    } catch (Exception $e){
+        $err = $e->getMessage();
+        echo '<br>'.$err;
+    
     }
-}
-
-//данные тестового паблика
-//$vk_group_id = 27421965;
-//$vk_aplication_id = 2842244; //id аппа
-//$vk_access_token = 'c7ac1842c3ddb889c3ddb88974c3f6e60dcc3ddc3d8801f85c16a3c7336abd4'; //
-//$vk_app_seckey = V1us1w3lbkoaapuYiddg;
-
-//заголовок - поле header
-try{
-    echo 'start<br>';
-    $a = new Vsend($post_array);
-    echo $a->send_post();
-
- 
-} catch (Exception $e){
-    $err = $e->getMessage();
-    echo '<br>'.$err;
-
-}
-?>
+    ?>
