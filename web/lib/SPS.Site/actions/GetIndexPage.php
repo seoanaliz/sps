fd7dbd7e2dd1ca101798974e2d5533c4fcc5b13d
Eugene Shulepin <eugene.shulepin@gmail.com>
2012-05-11 17:01:41 +0400

    багфикс отображения главной страницы

--- web/lib/SPS.Site/actions/GetIndexPage.php
+++ web/lib/SPS.Site/actions/GetIndexPage.php
@@ -26,17 +26,26 @@
             Response::setInteger('currentSourceFeedId', $currentSourceFeedId);
             Response::setInteger('currentTargetFeedId', $currentTargetFeedId);
 
-            $sourceFeeds = SourceFeedFactory::Get(
-                array('_sourceFeedId' => AccessUtility::GetSourceFeedIds($currentTargetFeedId))
-                , array( BaseFactory::WithoutPages => true )
-            );
-            Response::setArray( "sourceFeeds", $sourceFeeds );
-
             $targetFeeds = TargetFeedFactory::Get(
                 array('_targetFeedId' => AccessUtility::GetTargetFeedIds())
                 , array( BaseFactory::WithoutPages => true )
             );
             Response::setArray( "targetFeeds", $targetFeeds );
+
+            if (empty($currentTargetFeedId)) {
+                //пытаемся получить источники для первого паблика
+                if (!empty($targetFeeds)) {
+                    $currentTargetFeedId = current(array_keys($targetFeeds));
+                } else {
+                    $currentTargetFeedId = -1;
+                }
+            }
+
+            $sourceFeeds = SourceFeedFactory::Get(
+                array('_sourceFeedId' => AccessUtility::GetSourceFeedIds($currentTargetFeedId))
+                , array( BaseFactory::WithoutPages => true )
+            );
+            Response::setArray( "sourceFeeds", $sourceFeeds );
 
             $currentTimestamp = Session::getInteger('currentTimestamp');
             if (empty($currentTimestamp)) {
