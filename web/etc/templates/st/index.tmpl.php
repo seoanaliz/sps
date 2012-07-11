806b3a329727397ad8aad6301efcf6babc0fb7d3
Artyom Kohver <m.arko@yandex.ru>
2012-07-11 19:16:02 +0400

    Эмуляция взаимодействия клиент-сервер

--- web/etc/templates/st/index.tmpl.php
+++ web/etc/templates/st/index.tmpl.php
@@ -1,34 +1,11 @@
 {increal:tmpl://st/elements/header.tmpl.php}
+<div id="global-loader"></div>
 <div id="main" class="main">
     <div class="header">
-        <div class="tab-bar">
-            <span class="tab selected">Популярные</span>
-            <span class="tab">Список</span>
-        </div>
+        <div class="tab-bar"></div>
     </div>
     <div class="content">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="public" width="30%">
-                        <input class="filter" id="filter" type="text" placeholder="Поиск по названию" />
-                    </th>
-                    <th class="followers">
-                        подписчики
-                        <span class="icon arrow"></span>
-                    </th>
-                    <th class="growth">
-                        прирост
-                        <span class="icon arrow"></span>
-                    </th>
-                    <th class="contacts" width="31%">
-                        контакты
-                        <span class="icon arrow"></span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody id="table-body"></tbody>
-        </table>
+        <table class="table" id="table"></table>
     </div>
 </div>
 {increal:tmpl://st/elements/footer.tmpl.php}
--- web/shared/js/st/main.js
+++ web/shared/js/st/main.js
@@ -2,6 +2,7 @@
  * Events
  */
 var Events = {
+    delay: 400,
     eventList: {},
     fire: function(name, args){
         if (typeof args != "undefined") {
@@ -11,7 +12,13 @@ var Events = {
         }
         if ($.isFunction(this.eventList[name])) {
             try {
-                this.eventList[name].apply(window, args);
+                var t = this;
+                setTimeout(function() {
+                    console.log(name + ':');
+                    console.log(args.slice(0, -1));
+                    console.log('-------');
+                    t.eventList[name].apply(window, args);
+                }, t.delay);
             } catch(e) {
                 if(console && $.isFunction(console.log)) {
                     console.log(e);
@@ -20,148 +27,326 @@ var Events = {
         }
     }
 };
+
+/**
+ * Test data
+ */
+var DataUser = {
+    uid: 4718705,
+    first_name: 'Artyom',
+    last_name: 'Kohver'
+};
+
+var DataUsers = [ {
+        userId: 1,
+        userName: 'Asadasdfша Радославов',
+        userPhoto: 'http://cs4852.userapi.com/u78467867/d_689784aa.jpg',
+        userDescription: 'Сотрудничество'
+    }, {
+        userId: 2,
+        userName: 'Artyom Kohver',
+        userPhoto: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
+        userDescription: 'Сотрудничество'
+    }, {
+        userId: 3,
+        userName: 'Коля',
+        userPhoto: 'http://cs11368.userapi.com/u4373383/d_79f9e41f.jpg',
+        userDescription: 'Сотрудничество'
+    }, {
+        userId: 4,
+        userName: '123',
+        userPhoto: 'http://app.uxpin.com/u/a/0/9/a09e2c748e10258cd892f11307202c77/e_d7f264dc.jpg',
+        userDescription: 'Сотрудничество'
+    }
+];
+
+var DataTable = [ {
+        publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
+        publicName: 'Путешествия',
+        publicFollowers: '516',
+        publicGrowthNum: '1231',
+        publicGrowthPer: '0.53%',
+        users: DataUsers,
+
+        etc: false
+    }, {
+        publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
+        publicName: 'Креативные идеи',
+        publicFollowers: '123 456',
+        publicGrowthNum: '231',
+        publicGrowthPer: '0.53%',
+        users: DataUsers,
+
+        etc: false
+    }, {
+        publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
+        publicName: 'Тысяча чертей, какая',
+        publicFollowers: '55 111',
+        publicGrowthNum: '1',
+        publicGrowthPer: '0.53%',
+        users: DataUsers,
+
+        etc: false
+    }, {
+        publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
+        publicName: 'Эротика',
+        publicFollowers: '2 375',
+        publicGrowthNum: '2231',
+        publicGrowthPer: '0.53%',
+        users: DataUsers,
+
+        etc: false
+    }, {
+        publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
+        publicName: 'Самые красивые девушк',
+        publicFollowers: '71 375',
+        publicGrowthNum: '1131',
+        publicGrowthPer: '0.53%',
+        users: DataUsers,
+
+        etc: false
+    }, {
+        publicImg: 'http://app.uxpin.com/u/5/4/a/54a4c4259d2169e5bf80dfb906c03cf4/e_68ce31a0.jpg',
+        publicName: 'Интересные факты',
+        publicFollowers: '221 123',
+        publicGrowthNum: '0',
+        publicGrowthPer: '0%',
+        users: DataUsers,
+
+        etc: false
+    }
+];
+
+var DataList = [
+    {itemId: 1, itemTitle: 'Все', itemSelected: true},
+    {itemId: 2, itemTitle: 'Популярные'},
+    {itemId: 3, itemTitle: 'Прикольные'},
+    {itemId: 4, itemTitle: 'Смелые'},
+    {itemId: 5, itemTitle: 'Красивые'},
+    {itemId: 6, itemTitle: 'Ужасные'}
+];
+
+/**
+ * Initialization
+ */
 var Eventlist = {
-    load_stat: function(offset, limit, callback) {
+    load_list: function(viewer_id, callback) {
+        callback(false);
+    },
+    load_table: function(viewer_id, list_id, offset, limit, callback) {
+        callback(false);
+    },
+    add_list: function(viewer_id, title, callback) {
+        callback(false);
+    },
+    remove_list: function(viewer_id, list_id, callback) {
+        callback(false);
+    },
+    change_user: function(viewer_id, user_id, public_id, callback) {
         callback(false);
     }
 };
 $.extend(Events.eventList, Eventlist);
 
-/**
- * Test data
- */
-var Data = {
-    rows: [
-        {
-            publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
-            publicName: 'Путешествия',
-            publicFollowers: '271 375',
-            publicGrowthNum: '1231',
-            publicGrowthPer: '0.53%',
-            user: {
-                userId: 1,
-                userName: 'Саша Радославов',
-                userPhoto: 'http://app.uxpin.com/u/a/0/9/a09e2c748e10258cd892f11307202c77/e_d7f264dc.jpg',
-                userDescription: 'Сотрудничество'
-            },
+$(document).ready(function() {
+    Events.fire('load_list', [DataUser.uid, function(dataList) {
+        Events.fire('load_table', [DataUser.uid, 123, 0, 100, function(dataTable) {
+            updateList(dataList || DataList);
+            updateTable(dataTable || DataTable);
+            $('#global-loader').fadeOut(200);
+        }]);
+    }]);
+});
 
-            etc: false
-        },
-        {
-            publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
-            publicName: 'Креативные идеи',
-            publicFollowers: '271 375',
-            publicGrowthNum: '231',
-            publicGrowthPer: '0.53%',
-            user: {
-                userId: 1,
-                userName: 'Саша Радославов',
-                userPhoto: 'http://app.uxpin.com/u/a/0/9/a09e2c748e10258cd892f11307202c77/e_d7f264dc.jpg',
-                userDescription: 'Сотрудничество'
-            },
+function updateTable(dataDef) {
+    if (!dataDef.length) return;
 
-            etc: false
-        },
-        {
-            publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
-            publicName: 'Тысяча чертей, какая',
-            publicFollowers: '271 375',
-            publicGrowthNum: '1',
-            publicGrowthPer: '0.53%',
-            user: {
-                userId: 1,
-                userName: 'Саша Радославов',
-                userPhoto: 'http://app.uxpin.com/u/a/0/9/a09e2c748e10258cd892f11307202c77/e_d7f264dc.jpg',
-                userDescription: 'Сотрудничество'
-            },
+    var dataTable = dataDef.slice(0);
+    var $table;
+    var $tableHead;
+    var $tableBody;
+    var $filter;
 
-            etc: false
-        },
-        {
-            publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
-            publicName: 'Эротика',
-            publicFollowers: '271 375',
-            publicGrowthNum: '2231',
-            publicGrowthPer: '0.53%',
-            user: {
-                userId: 1,
-                userName: 'Саша Радославов',
-                userPhoto: 'http://app.uxpin.com/u/a/0/9/a09e2c748e10258cd892f11307202c77/e_d7f264dc.jpg',
-                userDescription: 'Сотрудничество'
-            },
+    updateElements();
+    renderTable({rows: dataTable});
 
-            etc: false
-        },
-        {
-            publicImg: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
-            publicName: 'Самые красивые девушк',
-            publicFollowers: '271 375',
-            publicGrowthNum: '1131',
-            publicGrowthPer: '0.53%',
-            user: {
-                userId: 1,
-                userName: 'Саша Радославов',
-                userPhoto: 'http://app.uxpin.com/u/a/0/9/a09e2c748e10258cd892f11307202c77/e_d7f264dc.jpg',
-                userDescription: 'Сотрудничество'
-            },
+    $table.delegate('.contact .content', 'click', function(e) {
+        var $el = $(this);
+        var offset = $el.offset();
+        var $dropdown = $el.data('dropdown');
+        e.stopPropagation();
 
-            etc: false
-        },
-        {
-            publicImg: 'http://app.uxpin.com/u/5/4/a/54a4c4259d2169e5bf80dfb906c03cf4/e_68ce31a0.jpg',
-            publicName: 'Интересные факты',
-            publicFollowers: '221 375',
-            publicGrowthNum: '0',
-            publicGrowthPer: '0%',
-            user: {
-                userId: 1,
-                userName: 'Саша Радославов',
-                userPhoto: 'http://cs407727.userapi.com/v407727814/2b33/ui72j20Zn9w.jpg',
-                userDescription: 'Сотрудничество'
-            },
+        if (!$el.hasClass('selected')) {
+            $el.addClass('selected');
+            if (!$dropdown) {
+                var dataItems = DataUsers;
+                $dropdown = $(tmpl(CONTACT_DROPDOWN, {users: dataItems})).appendTo('body');
 
-            etc: false
-        }
-    ]
-};
-
-/**
- * Initialization
- */
-Events.fire('load_stat', [1, 1, function(data) {
-    init(data || Data);
-}]);
-
-function init(defData) {
-    $(document).ready(function() {
-        updateTable(defData);
-
-        $('#filter').keyup(function() {
-            var rows = $.grep(defData.rows, function(n) {
-                return n.publicName.toLowerCase().indexOf($('#filter').val().toLowerCase()) != -1;
-            });
-            updateTable({rows: rows});
-        });
-
-        $('.growth').click(function() {
-            var rows;
-
-            $(this).toggleClass('active');
-            if ($(this).hasClass('active')) {
-                rows = defData.rows.slice(0).sort(function(first, second) {
-                    var a = parseInt(first.publicGrowthNum);
-                    var b = parseInt(second.publicGrowthNum);
-                    return (a > b) ? -1 : ((a < b) ? 1 : 0);
+                $dropdown.delegate('.item', 'mousedown', function(e) {
+                    var $item = $(this);
+                    $dropdown.find('.item.selected').removeClass('selected');
+                    $item.addClass('selected');
+                    onChange($item);
                 });
-            } else {
-                rows = defData.rows;
-            }
-            updateTable({rows: rows});
-        });
+                $dropdown.bind('mousedown', function(e) {
+                    e.stopPropagation();
+                });
+                $(document).mousedown(function() {
+                    if ($dropdown.is(':hidden')) return;
+                    $dropdown.hide();
+                    $el.removeClass('selected');
+                });
 
-        function updateTable(data) {
-            $('#table-body').html(tmpl(TABLE_BODY, data));
+                function onChange($item) {
+                    Events.fire('change_user', [DataUser.uid, $item.data('user-id'), 123, function(data) {
+                        //$el.closest('.contact').html('');
+                    }]);
+                }
+                $el.data('dropdown', $dropdown);
+            }
+            $dropdown.show().css({
+                top: offset.top + $el.outerHeight(),
+                left: offset.left - 1,
+                width: $el.outerWidth()
+            });
         }
+    });
+
+    $table.delegate('.action.add-to-list', 'click', function(e) {
+        var $el = $(this);
+        var offset = $el.offset();
+        var $dropdown = $el.data('dropdown');
+        e.stopPropagation();
+
+        Events.fire('load_list', [DataUser.uid, function(dataList) {
+            if (!$el.hasClass('selected')) {
+                $el.addClass('selected');
+                if (!$dropdown) {
+                    var dataItems = dataList || DataList;
+                    $dropdown = $(tmpl(DROPDOWN, {items: dataItems})).appendTo('body');
+                    var $input = $dropdown.find('input');
+
+                    $dropdown.delegate('.item', 'mousedown', function(e) {
+                        var $item = $(this);
+                        $item.toggleClass('selected');
+                        onChange($item);
+                    });
+                    $dropdown.bind('mousedown', function(e) {
+                        e.stopPropagation();
+                    });
+                    $(document).mousedown(function() {
+                        if ($dropdown.is(':hidden')) return;
+                        $dropdown.hide();
+                        $el.removeClass('selected');
+                    });
+                    $input.bind('keyup', function(e) {
+                        var text = $.trim($input.val());
+                        if (e.keyCode && e.keyCode != 13) return false;
+                        if (!text) return false;
+                        if (e.keyCode == 13) return onSave(text);
+                    });
+
+                    function onSave(text) {
+                        Events.fire('add_list', [DataUser.uid, text, function(data) {
+                            $input.val('').before(tmpl(DROPDOWN_ITEM, {itemId: 1, itemTitle: text}));
+                        }]);
+                    }
+                    function onChange($item) {
+                        $el.find('.icon').addClass('select');
+                    }
+                    $el.data('dropdown', $dropdown);
+                }
+                $dropdown.show().css({
+                    top: offset.top + $el.outerHeight(),
+                    left: offset.left - $dropdown.outerWidth() + $el.outerWidth()
+                });
+            }
+        }]);
+    });
+
+    $('.followers').click(function() {
+        sortTable(this, 'publicFollowers');
+    });
+
+    $('.growth').click(function() {
+        sortTable(this, 'publicGrowthNum');
+    });
+
+    $('.contacts').click(function() {
+        sortTable(this, function(a) {
+            return a.user['userName'].toLowerCase();
+        });
+    });
+
+    $filter.keyup(function() {
+        dataTable = $.grep(dataDef, function(n) {
+            return n.publicName.toLowerCase().indexOf($filter.val().toLowerCase()) != -1;
+        });
+        $tableHead.find('th.active').removeClass('active');
+        renderTableBody({rows: dataTable});
+    });
+
+    function sortTable(target, index) {
+        var rows;
+        var reverse = -1;
+        var $target = $(target);
+        var parse = $.isFunction(index) ? index : function(a) {
+            return parseInt(a[index].split(' ').join(''));
+        };
+        var sort = function(first, second) {
+            var a = parse(first);
+            var b = parse(second);
+            return (a > b) ? reverse : ((a < b) ? -reverse : 0);
+        };
+
+        $tableHead.find('th.active').not($target).removeClass('active');
+        if (!$target.hasClass('active')) {
+            $target.addClass('active');
+            rows = dataTable.slice(0).sort(sort);
+        } else if (!$target.hasClass('reverse')) {
+            $target.addClass('reverse');
+            reverse = -reverse;
+            rows = dataTable.slice(0).sort(sort);
+        } else {
+            $target.removeClass('active');
+            $target.removeClass('reverse');
+            rows = dataTable;
+        }
+        renderTableBody({rows: rows});
+    }
+
+    function updateElements() {
+        $table = $('#table');
+        $tableHead = $('#table-head');
+        $tableBody = $('#table-body');
+        $filter = $('#filter');
+        $table.unbind();
+        $filter.unbind();
+    }
+
+    function renderTable(data) {
+        $table.empty().html(tmpl(TABLE, data));
+        updateElements();
+    }
+    function renderTableBody(data) {
+        $tableBody.empty().html(tmpl(TABLE_BODY, data));
+    }
+}
+
+function updateList(dataDef) {
+    if (!dataDef.length) return;
+
+    var dataList = dataDef.slice(0);
+
+    $('.header').empty().html(tmpl(LIST, {items: dataList}));
+    $('.header').delegate('.tab', 'click', function() {
+        $('.header .tab').removeClass('selected');
+        var $item = $(this);
+        $item.addClass('selected');
+
+        Events.fire('load_table', [DataUser.uid, $item.data('id'), 0, 100, function(dataTable) {
+            updateTable(dataTable || DataTable);
+        }]);
     });
 }
 
@@ -203,6 +388,37 @@ function init(defData) {
     return t.tmpl = tmpl;
 })();
 
+var LIST =
+'<div class="tab-bar">' +
+    '<? each(LIST_ITEM, items); ?>';
+'</div>';
+
+var LIST_ITEM =
+'<span data-id="<?=itemId?>" class="tab<?=isset("itemSelected") ? " selected" : "" ?>">' +
+    '<?=itemTitle?>' +
+'</span>';
+
+var TABLE =
+'<thead id="table-head">' +
+    '<tr>' +
+        '<th class="public" width="30%">' +
+            '<input class="filter" id="filter" type="text" placeholder="Поиск по названию" />' +
+        '</th>' +
+        '<th class="followers">' +
+            'подписчики<span class="icon arrow"></span>' +
+        '</th>' +
+        '<th class="growth">' +
+            'прирост<span class="icon arrow"></span>' +
+        '</th>' +
+        '<th class="contacts" width="31%">' +
+            'контакты<span class="icon arrow"></span>' +
+        '</th>' +
+    '</tr>' +
+'</thead>' +
+'<tbody id="table-body">' +
+    '<?=tmpl(TABLE_BODY, {rows: rows})?>'
+'</tbody>';
+
 var TABLE_BODY =
 '<? each(TABLE_ROW, rows); ?>';
 
@@ -221,7 +437,12 @@ var TABLE_ROW =
         '</span>' +
     '</td>' +
     '<td>' +
-        '<?=tmpl(CONTACT, user)?>' +
+        '<?=tmpl(CONTACT, users[0])?>' +
+        '<div class="actions">' +
+            '<span class="action add-to-list">' +
+                '<span class="icon plus"></span>' +
+            '</span>' +
+        '</div>' +
     '</td>' +
 '</tr>';
 
@@ -237,13 +458,35 @@ var CONTACT =
         '<div class="description">' +
             '<?=userDescription?>' +
         '</div>' +
+        '<div class="icon arrow"></div>' +
     '</div>' +
-    '<div class="actions">' +
-        '<span class="action">' +
-            '<span class="icon arrow"></span>' +
-        '</span>' +
-        '<span class="action">' +
-            '<span class="icon plus"></span>' +
-        '</span>' +
+'</div>';
+
+var DROPDOWN =
+'<div class="dropdown">' +
+    '<? each(DROPDOWN_ITEM, items); ?>' +
+    '<input type="text" class="add-item" placeholder="Создать список" />';
+'</div>';
+
+var DROPDOWN_ITEM =
+'<div data-id="<?=itemId?>" class="item"><?=itemTitle?><div class="icon plus"></div></div>';
+
+var CONTACT_DROPDOWN =
+'<div class="contact-dropdown">' +
+    '<? each(CONTACT_DROPDOWN_ITEM, users); ?>' +
+'</div>';
+
+var CONTACT_DROPDOWN_ITEM =
+'<div class="item" data-user-id="<?=userId?>">' +
+    '<div class="photo">' +
+        '<img src="<?=userPhoto?>" alt="" />' +
+    '</div>' +
+    '<div class="content">' +
+        '<div class="name">' +
+            '<a target="_blank" href="http://vk.com/im?sel=<?=userId?>"><?=userName?></a>' +
+        '</div>' +
+        '<div class="description">' +
+            '<?=userDescription?>' +
+        '</div>' +
     '</div>' +
 '</div>';
