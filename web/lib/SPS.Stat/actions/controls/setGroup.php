af84d08e1aa86b1f20274be288bda2d5cae4196f
akalie <askaslie@gmail.com>
2012-07-13 16:35:27 +0400

    Update akalie

--- web/lib/SPS.Stat/actions/controls/setGroup.php
+++ web/lib/SPS.Stat/actions/controls/setGroup.php
@@ -6,69 +6,28 @@
      * @package    SPS
      * @subpackage Stat
      */
-    class setGroup extends wrapper {
+    class setPostPrice extends wrapper {
 
         /**
          * Entry Point
          */
         public function Execute() {
-            $publId     =   Request::getInteger( 'publId' );
-            $userId     =   Request::getInteger( 'userId' );
-            $groupId    =   Request::getInteger( 'groupId' );
-            $groupName  =   Request::getString ( 'groupName' );
-            $groupName  =   $groupName ? $groupName : 0;
-            if (!$publId || !$userId) {
+            $publId = Request::getInteger( 'publId' );
+            $price  = Request::getInteger( 'price' );
 
+            if (empty($publId)) {
                 echo ObjectHelper::ToJSON(array('response' => false));
                 die();
             }
+            $price = $price ? $price : 0;
 
-            $query = 'SELECT * FROM groups WHERE "name"=@name AND user_id=@userId';
+            $query = 'UPDATE publs50k SET price=@price WHERE vk_id=@publ_id';
             $cmd = new SqlCommand( $query, ConnectionFactory::Get('tst') );
-            $cmd->SetInteger('@userId', $userId);
-            $cmd->SetString('@name',    $groupName);
+            $cmd->SetInteger('@publ_id',    $publId);
+            $cmd->SetInteger('@price',   $price);
+            $cmd->Execute();
 
-            $ds = $cmd->Execute();
-            print_r($ds);
-            $ds->next();
-            echo '<br>';
-            print_r($ds);
-            if($ds->getValue('name')) {
-                echo ObjectHelper::ToJSON(array('response' => false));
-                die();
-            }
-
-            //rename
-            if ($groupId) {
-                $query = 'UPDATE groups SET "name"=@name WHERE group_id=@group_id';
-                $cmd = new SqlCommand( $query, ConnectionFactory::Get('tst') );
-                $cmd->SetInteger('@group_id',   $groupId);
-                $cmd->SetString('@name',        $groupName);
-                $cmd->Execute();
-
-                echo $query;
-            //new
-            } elseif($groupName) {
-                $query = 'INSERT INTO groups("name",user_id) VALUES(@name, @user_id) RETURNING group_id';
-                $cmd = new SqlCommand( $query, ConnectionFactory::Get('tst') );
-                $cmd->SetInteger('@user_id', $userId);
-                $cmd->SetString('@name',     $groupName);
-                $ds = $cmd->Execute();
-                $ds->next();
-                print_r($ds);
-                $id = $ds->getValue('group_id', TYPE_INTEGER);
-
-                $query = 'INSERT INTO publ_rels_names(user_id,publ_id,group_id) VALUES(@user_id,@publ_id,@group_id)';
-                $cmd = new SqlCommand( $query, ConnectionFactory::Get('tst') );
-                $cmd->SetInteger('@user_id', $userId);
-                $cmd->SetInteger('@publ_id', $publId);
-                $cmd->SetInteger('@group_id',$id);
-                $cmd->Execute();
-
-//
-            }
             echo ObjectHelper::ToJSON(array('response' => true));
-
         }
     }
 
