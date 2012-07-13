d046dfb0a63152b7dcf3b04c396814514de8df5a
akalie <askaslie@gmail.com>
2012-07-13 16:34:05 +0400

    Update akalie

--- web/lib/SPS.Stat/actions/controls/deleteGroup.php
+++ web/lib/SPS.Stat/actions/controls/deleteGroup.php
@@ -12,16 +12,24 @@
          * Entry Point
          */
         public function Execute() {
-            $userId     = Request::getInteger( 'userId' );
-            $groupName  = Request::getString ( 'groupName' );
-            if (!$userId || !$groupName) {
+            //$userId   = Request::getInteger( 'userId' );
+            $groupId  = Request::getInteger ( 'groupId' );
+            if (!$groupId) {
                 echo  ObjectHelper::ToJSON(array('response' => false));
-                return;
+                die();
             }
+            echo '123123';
+            $query = 'DELETE FROM publ_rels_names WHERE group_id=@group_id';
+            $cmd = new SqlCommand( $query, ConnectionFactory::Get('tst') );
+            $cmd->SetInteger('@group_id', $groupId);
+            $cmd->Execute();
 
-            $query = sprintf('DELETE FROM publ_rels_names WHERE user_id=%1$d AND group_name=\'%2$s\''
-                , $userId, $groupName);
-            $this->db_wrap('query', $query);
+
+            $query = 'DELETE FROM groups WHERE group_id=@group_id';
+            $cmd = new SqlCommand( $query, ConnectionFactory::Get('tst') );
+            $cmd->SetInteger('@group_id', $groupId);
+            $cmd->Execute();
+
             echo  ObjectHelper::ToJSON(array('response' => true));
         }
     }
