d18fa953b7c6803633e65b17d8cc2a68a4fff4ae
Artyom Kohver <m.arko@yandex.ru>
2012-07-26 15:32:42 +0400

    wall_load без offset и limit

--- web/etc/templates/app/elements/menu.tmpl.php
+++ web/etc/templates/app/elements/menu.tmpl.php
@@ -1,18 +1,9 @@
 <div class="menu" id="menu">
-    <div class="item my selected">
+    <div class="item my selected" data-id="my1">
         <div class="content">
             My publications
             <span class="counter">+1</span>
         </div>
     </div>
     {increal:tmpl://app/elements/menu-item.tmpl.php}
-    {increal:tmpl://app/elements/menu-item.tmpl.php}
-    {increal:tmpl://app/elements/menu-item.tmpl.php}
-    <div class="item user">
-        <div class="photo"><img src="http://vk.cc/Q2PuP" alt="" /></div>
-        <div class="content">
-            <div class="text">Artyom Kohver</div>
-            <div class="description">General editor</div>
-        </div>
-    </div>
 </div>
