8bddb5735da9a42ff7c87024043340d527c178e9
Euegene Shulepin <eugene.shulepin@gmail.com>
2012-07-26 16:54:04 +0400

    отображение списка авторских постов

--- web/etc/templates/app/elements/wall-posts-list.tmpl.php
+++ web/etc/templates/app/elements/wall-posts-list.tmpl.php
@@ -1,4 +1,5 @@
 <!--Ajax template-->
 {increal:tmpl://app/elements/wall-post.tmpl.php}
-{increal:tmpl://app/elements/wall-post.tmpl.php}
+<? if ($hasMore) {?>
 <div id="wall-show-more" class="show-more">Show more posts</div>
+<? } ?>
