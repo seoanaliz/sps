2b57bf419b9ace9e95c2a6e3d67c0b1f315d0824
akalie <askaslie@gmail.com>
2012-08-01 17:10:36 +0400

    msc

--- web/etc/templates/vt/stat_admins/index.tmpl.php
+++ web/etc/templates/vt/stat_admins/index.tmpl.php
@@ -1,101 +1,73 @@
-{increal:tmpl://vt/header.tmpl.php}
-
-<script type="text/javascript">
-    function setPrice(a){
-        var $input  =   a.find('input');
-        var $price  =   Number($input.val());
-        var $id     =   $(a).prop('id');
-
-        if (isNaN($price) || $price == 0){return;}
-
-//        alert(controlsRoot + 'setPrice/');
-            $.ajax({
-                    url: controlsRoot + 'setPrice/',
-                    type: 'GET',
-//                    dataType : "json",
-                    data: {
-                        publId: $id,
-                        price: $price
-                    },
-                    success: function (data) {
-                        alert('ok');
-                    }
-            });
-    };
 
 
-</script>
- <table id="tbld" class="tablesorter" bordercolor="black" border="1"  >
-    <thead>
-    <tr>
-        <th>Паблик</th>
-        <th>Название</th>
-        <th> <?=Response::getInteger('last_time'); ?> </th>
-        <th>Процент роста</th>
-        <th>Абсолютный рост</th>
-        <th>Администраторы</th>
-    </tr>
-    </thead>
-    <tbody>;
+<form>
+    Выберите дату начала: <input type="date" name="from">
+    Выберите дату конца:  <input type="date" name="to">
+    <input type="submit" value="Отправить"></p>
+</form>
 
+<?
+    $time_start = Request::getDateTime('from');
+    $time_start = $time_start ? $time_start->getTimestamp() : 0 ;
+    $time_stop = Request::getDateTime('to');
+    $time_stop = $time_stop ? $time_stop->getTimestamp() : 0 ;
 
-     <?
+    foreach( $our_publics as $public ) {
+        $admins = AdminsWork::get_public_admins($public, $time_start, $time_stop);
+        if( !$admins )
+            continue;
+     ?>
 
+        <h3><a href="http://vk.com/public<?=$public['id']?>"> <?=$public['title']?></a></h3>
+        <table width="100" border="1 black">
+        <thead>
+            <td>Админ</td>
+            <td>Постов</td>
+            <td>Топиков</td>
+            <td>Сложных</td>
+            <td>Репост/пост</td>
+            <td>Лайк/пост</td>
+            <td>Список постов</td>
+        </thead>
+    <?
 
-             $button_form = '<br>
-                       <div id="%1$d" class="price" >
-                            <br> %2$d р.<br>
-                            <input type="text" size="10"> р.<br>
-                            <button type="button" onclick="javascript:setPrice($(this).parent());" >Послать</button>
-                       </div>
-             ';
+        foreach( $admins as $admin ) {
 
-    for($i = 1; $i <= $pages; $i++)
-    {
-        echo '<a href="' . '?page=' . $i . '">' . $i . '</a>  ';
-        echo '  ';
+            $posts = AdminsWork::get_posts($admin['id'], $public['id'], $time_start, $time_stop);
+            if(!$posts)
+                continue;
+            ?>
+            <tr>
+            <td><a href="http://vk.com/id<?=$admin['id'];?>"><img src="<?=$admin['ava']?>"/><br><?=$admin['name']?> </a></td>
+            <td><?=count($posts)-4;?></td>
+            <td><?=$posts['topics']?></td>
+            <td><?=$posts['compls']?></td>
+            <td><?=$posts['reposts']?></td>
+            <td><?=$posts['rel_likes']?></td>
+            <td>
+            <?
+                unset ($posts[topics]);
+                unset ($posts[compls]);
+                unset ($posts[reposts]);
+                unset ($posts[rel_likes]);
+//            foreach($posts as $post) {
+//                ?>
+<!--                    <a href="http://vk.com/wall-"--><?//=$public['id']?><!--_--><?//=$post?><!-->--><?//=$public['id']?><!--_--><?//=$post?><!--</a>-->
+<!---->
+<!---->
+<!--                --><?//
+//            }
+            ?>
+            </td></tr>
+            <?
+        }
+        ?>
+        </table><br><br>
+        <?
     }
-    echo '<br>';
-    echo '<br>';
 
-        foreach($big_publics_array as $publ => $dt) {
-            $price = $dt['price'];
-            $time_last = end($dt['quantity']);
-            $time_comparison = prev($dt['quantity']);
-            if(!$time_comparison)
-                $time_comparison = $time_last;
-            if (!$time_comparison || !$time_last) continue;
-            echo '<tr>';
-            $img = "<img src=\"". $dt['ava'] . "\" href=\"http://vk.com/public$publ\"/>";
-            $ad_line = '';
 
-            //формирование ячейки админов
-            if (count($dt['admins']) > 0) {
-                foreach ($dt['admins'] as $admin) {
-                    $ad_line .= "<a href=\"http://vk.com/id"
-                             . $admin['vk_id'] . "\"><img width=50 src=\""
-                             . $admin['ava']. "\" title=\"" .  $admin['name']. " "
-                             . $admin['role'] . "\"  /></a>"
 
-                             ."<br>";
-                }
-            }
-
-            $ad_line = $ad_line ? $ad_line : 'Данные отсутствуют';
-
-            $name = "<a  href=\"http://vk.com/public$publ\">" . $dt['name'] . "</a>";
-            echo "<td >$img<td>$name  <td> $time_last<td>" . round($time_last*100/$time_comparison-100, 2) . '%<td>' .( $time_last - $time_comparison).'<td>'
-                . $ad_line . sprintf($button_form, $publ, $dt['price']) . '</td>';
-            echo '</tr>';
-        }
-        echo '</tbody></table>';
-           echo '<br>';
-           echo '<br>';
-
-        for($i = 1; $i <= $pages; $i++)
-        {
-            echo '<a href="' . '?page=' . $i . '">' . $i . '</a> ';
-            echo '  ';
-        }
 ?>
+
 {increal:tmpl://vt/footer.tmpl.php}
