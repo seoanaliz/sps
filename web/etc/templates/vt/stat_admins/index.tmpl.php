fa8c4595b4c75e349158bb66e32ff6762ae7942b
akalie <askaslie@gmail.com>
2012-08-01 19:09:46 +0400

    msc v1.215

--- web/etc/templates/vt/stat_admins/index.tmpl.php
+++ web/etc/templates/vt/stat_admins/index.tmpl.php
@@ -10,10 +10,11 @@
 
     $time_start = Request::getDateTime('from');
     $time_start = $time_start ? $time_start->getTimestamp() : 0 ;
-    $time_stop = Request::getDateTime('to');
-    $time_stop = $time_stop ? $time_stop->getTimestamp() : 0 ;
+    $time_stop  = Request::getDateTime('to');
+    $time_stop  = $time_stop ? $time_stop->getTimestamp() : 0 ;
 
-    foreach( $our_publics as $public ) {
+    foreach( $our_publics as $public )
+    {
         $admins = AdminsWork::get_public_admins($public, $time_start, $time_stop);
         if( !$admins )
             continue;
@@ -67,7 +68,64 @@
         <?
     }
 
+    $public = -1;
+    $admins = AdminsWork::get_public_admins($time_start, $time_stop);
+    print_r($admins);
+    foreach( $admins as $admin )
+    {
+        $admins = AdminsWork::get_public_admins($public, $time_start, $time_stop);
+        if( !$admins )
+            continue;
+        ?>
 
+        <h3><a href="http://vk.com/id<?=$admin['id'];?>"><img src="<?=$admin['ava']?>"/><br><?=$admin['name']?> </a></h3>
+        <table width="100" border="1 black">
+        <thead>
+        <td>Паблик</td>
+        <td>Постов</td>
+        <td>Топиков</td>
+        <td>Сложных</td>
+        <td>Репост/пост</td>
+        <td>Лайк/пост</td>
+        <td>Список постов</td>
+        </thead>
+
+        <?
+
+        foreach( $publics as $public ) {
+
+            $posts = AdminsWork::get_posts($admin['id'], $public['id'], $time_start, $time_stop);
+            if(!$posts)
+                continue;
+            ?>
+            <tr>
+                <td><a href="http://vk.com/public<?=$public['id']?>"> <?=$public['title']?></a></td>
+                <td><?=count($posts)-4;?></td>
+                <td><?=$posts['topics']?></td>
+                <td><?=$posts['compls']?></td>
+                <td><?=$posts['reposts']?></td>
+                <td><?=$posts['rel_likes']?></td>
+                <td>
+                    <?
+    //                unset ($posts[topics]);
+    //                unset ($posts[compls]);
+    //                unset ($posts[reposts]);
+    //                unset ($posts[rel_likes]);
+    //            foreach($posts as $post) {
+    //                ?>
+                    <!--                    <a href="http://vk.com/wall-"--><?//=$public['id']?><!--_--><?//=$post?><!-->--><?//=$public['id']?><!--_--><?//=$post?><!--</a>-->
+                    <!---->
+                    <!---->
+                    <!--                --><?//
+    //            }
+                    ?>
+                </td></tr>
+            <?
+        }
+        ?>
+        </table><br><br>
+<?
+    }
 
 ?>
 
