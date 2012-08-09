8821d575728e6802bebf5fa115f6b588863a7a74
Eugene Shulepin <eugene.shulepin@gmail.com>
2012-08-09 12:07:48 +0400

    #6674 логика сохранения счетчиков новых комментариев и отправок постов

---
+++ web/lib/SPS.App/AuthorEvent.php
@@ -0,0 +1,33 @@
+<?php
+    /**
+     * WTF MFD EG 1.6 [t:trunk]
+     * Copyright (c) The 1ADW. All rights reserved.
+     */
+
+    /**
+     * AuthorEvent
+     *
+     * @package SPS
+     * @subpackage App
+     */
+    class AuthorEvent {
+
+        /** @var int */
+        public $articleId;
+
+        /** @var Article */
+        public $article;
+
+        /** @var int */
+        public $authorId;
+
+        /** @var Author */
+        public $author;
+
+        /** @var array */
+        public $commentIds;
+
+        /** @var bool */
+        public $isSent;
+    }
+?>
---
+++ web/lib/SPS.App/AuthorEventUtility.php
@@ -0,0 +1,64 @@
+<?php
+    /**
+     * AuthorEventUtility
+     * @package    SPS
+     * @subpackage App
+     * @author     Shuler
+     */
+    class AuthorEventUtility {
+
+        public static function EventQueue(Article $article) {
+            $event = new AuthorEvent();
+            $event->articleId = $article->articleId;
+            $event->authorId = $article->authorId;
+            $event->isSent = true;
+
+            //плохо, но у нас не отлавливается duplicate PK, а upsert не хочется писать
+            $result = @AuthorEventFactory::Add($event);
+            if (!$result) {
+                AuthorEventFactory::UpdateByMask($event, array('isSent'), array('articleId' => $event->articleId));
+            }
+        }
+
+        public static function EventComment(Article $article, $commentId) {
+            $event = new AuthorEvent();
+            $event->articleId = $article->articleId;
+            $event->authorId = $article->authorId;
+            $event->isSent = false;
+            $event->commentIds[] = $commentId;
+
+            $result = @AuthorEventFactory::Add($event);
+            if (!$result) {
+                $sql = <<<sql
+                  UPDATE "authorEvents" SET "commentIds" = array_append("commentIds", @commentId)
+                  WHERE "articleId" = @articleId
+sql;
+                $cmd = new SqlCommand($sql, ConnectionFactory::Get());
+                $cmd->SetInt('@commentId', $commentId);
+                $cmd->SetInt('@articleId', $article->articleId);
+                $cmd->ExecuteNonQuery();
+            }
+        }
+
+        public static function GetAuthorCounter($authorId) {
+            $result = 0;
+
+            $sql = <<<sql
+                SELECT
+                    SUM((CASE WHEN "isSent" = true THEN 1 ELSE 0 END + coalesce(array_length("commentIds", 1), 0))) as "counter"
+                FROM "authorEvents"
+                WHERE "authorId" = @authorId
+sql;
+
+            $cmd = new SqlCommand($sql, ConnectionFactory::Get());
+            $cmd->SetInt('@authorId', $authorId);
+            $ds = $cmd->Execute();
+
+            while ($ds->Next()) {
+                $result = $ds->GetInteger('counter');
+            }
+
+            return $result;
+        }
+    }
+?>
