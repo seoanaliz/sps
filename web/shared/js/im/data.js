e7e73d732b8fe48774f8832d7b3bf44d58b662c5
Artyom Kohver <m.arko@yandex.ru>
2012-08-09 18:48:49 +0400

    Добавлена Data

--- web/shared/js/im/data.js
+++ web/shared/js/im/data.js
@@ -1,14 +1,18 @@
 var Data = {
+    users: [
+        {
+            id: 4718705,
+            name: 'Artyom Kohver',
+            photo: 'http://vk.com/images/camera_c.gif',
+            isOnline: true
+        }
+    ],
     dialogs: [
         {
             id: 1,
-            user: {
-                id: 4718705,
-                name: 'Artyom Kohver',
-                photo: 'http://vk.com/images/camera_c.gif',
-                isOnline: true
-            },
+            user: Data.users[0],
             lastMessage: {
+                user: Data.users[0],
                 text: 'Hello!',
                 timestamp: 1234567890
             }
@@ -18,7 +22,23 @@ var Data = {
         {
             id: 1,
             title: 'Item',
-            icon: 'http://vk.com/images/camera_c.gif'
+            icon: 'http://vk.com/images/camera_c.gif',
+            dialogs: [
+                {
+                    id: 1,
+                    user: Data.users[0]
+                }
+            ]
+        }, {
+            id: 2,
+            title: 'Test',
+            icon: 'http://vk.com/images/camera_c.gif',
+            dialogs: [
+                {
+                    id: 1,
+                    user: Data.users[0]
+                }
+            ]
         }
     ]
 };
