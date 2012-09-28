50d0dc429ad8b439326b45d5b35fde7935f9e4fe
akalie <askaslie@gmail.com>
2012-09-28 15:55:44 +0400

    если параметр search не задан или 0, вернет все шаблоны листа

--- web/lib/SPS.Stat/actions/controls/messager/findTemplate.php
+++ web/lib/SPS.Stat/actions/controls/messager/findTemplate.php
@@ -9,14 +9,16 @@ class findTemplate
 
         $user_id        =   Request::getInteger( 'userId' );
         $search         =   Request::getString ( 'search'   );
-        $group_id      =   Request::getstring ( 'groupId' );
+        $group_id       =   Request::getstring ( 'groupId' );
 
-        if ( !$user_id || !$search ) {
+        if ( !$user_id  ) {
             die(ERR_MISSING_PARAMS);
         }
 
-        if (!$group_id)
-            $group_id = '0';
+
+        $group_id   = $group_id ? $group_id : '0';
+        $search     = $search   ? $search   : '0';
+
         $res = MesDialogs::search_template( $search, $group_id );
 
         echo ObjectHelper::ToJSON( array( 'response' => $res ));
