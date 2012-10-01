d49673cd4e74b8ec9b15604881545ce6d00305ce
Artyom Kohver <m.arko@yandex.ru>
2012-10-01 15:01:58 +0400

    Login cookie fix

--- web/shared/js/im/main.js
+++ web/shared/js/im/main.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     }
 
     if (!Configs.vkId) {
-        return location.replace('/login/');
+        return location.replace('/login/?' + btoa('im'));
     } else {
         $.cookie('uid', Configs.vkId, {expires: 30});
     }
