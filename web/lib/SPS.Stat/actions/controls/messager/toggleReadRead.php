520c52e3cbd307f932b09f0870d26339231f3882
akalie <askaslie@gmail.com>
2012-10-01 17:27:48 +0400

    msc

--- web/lib/SPS.Stat/actions/controls/messager/toggleReadRead.php
+++ web/lib/SPS.Stat/actions/controls/messager/toggleReadRead.php
@@ -5,13 +5,13 @@ class toggleReadRead
 {
     public function execute()
     {
-//        error_reporting( 0 );
+        error_reporting( 0 );
 
         $user_id        =   Request::getInteger( 'userId' );
         $group_id       =   Request::getInteger( 'groupId' );
         $read           =   Request::getBoolean( 'read' );
 
-        if ( !$user_id || !$group_id) {
+        if ( !$user_id || !$group_id ) {
             die(ERR_MISSING_PARAMS);
         }
 
