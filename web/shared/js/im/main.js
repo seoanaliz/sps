3d4b58f2776129b5f46464ebc3e4f49d77eb8ce3
kohver <m.arko@yandex.ru>
2012-10-29 16:42:42 +0400

    Фикс

--- web/shared/js/im/main.js
+++ web/shared/js/im/main.js
@@ -1008,6 +1008,8 @@ var RightColumn = Widget.extend({
                     setAsRead = true;
                     item.isRead(true);
                 }
+            } else {
+                item.isSelected(false);
             }
         }
         t.renderTemplate();
