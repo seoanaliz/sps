ba246949807468160928b7a25a53ab5d3745a513
Eugene Shulepin <eugene.shulepin@gmail.com>
2012-11-13 14:20:32 +0400

    #9420 скрипт обновления тестовых баз

--- docs/update-test-db.sh
+++ docs/update-test-db.sh
@@ -1,3 +1,7 @@
 pg_dump -U sps -O -f /tmp/tst.dump.sql tst
 echo "drop schema public cascade; create schema public" | psql -U sps tst-beta
 psql -U sps -f /tmp/tst.dump.sql tst-beta
+
+pg_dump -U sps -O -f /tmp/sps.dump.sql sps
+echo "drop schema public cascade; create schema public" | psql -U sps sps-beta
+psql -U sps -f /tmp/sps.dump.sql sps-beta
