fafa042597d78cafa819afe962b765ef294adb71
kohver <m.arko@yandex.ru>
2012-11-20 17:25:09 +0400

    Clear diff

--- web/etc/templates/im/index.tmpl.php
+++ web/etc/templates/im/index.tmpl.php
@@ -1,5 +1,3 @@
 {increal:tmpl://im/elements/header.tmpl.php}
-<div id="scroll-fix">
-    <div id="main" class="main clear-fix"></div>
-</div>
+<div id="main" class="main clear-fix"></div>
 {increal:tmpl://im/elements/footer.tmpl.php}
