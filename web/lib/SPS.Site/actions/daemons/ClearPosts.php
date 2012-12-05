58417b2d49b503dc3a1a068fda53dd22c4057c7e
Eugene Shulepin <eugene.shulepin@gmail.com>
2012-12-05 14:45:51 +0400

    #10622 увеличиваем лимит постов для очистки

--- web/lib/SPS.Site/actions/daemons/ClearPosts.php
+++ web/lib/SPS.Site/actions/daemons/ClearPosts.php
@@ -22,7 +22,7 @@
                 where a."statusId" = 3 and aq."articleId" IS NULL
                 and "isCleaned" = false
                 order by a."articleId"
-                limit 1000;
+                limit 5000;
 sql;
 
             $cmd = new SqlCommand($sql, ConnectionFactory::Get());
