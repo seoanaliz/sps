5c997f53870784fbd1cda16e13cba275e9fd3318
Eugene Shulepin <eugene.shulepin@gmail.com>
2012-12-05 17:15:00 +0400

    #10622 Скрипт удаления фотографий ненужных постов

--- web/lib/SPS.Site/actions/daemons/ClearPosts.php
+++ web/lib/SPS.Site/actions/daemons/ClearPosts.php
@@ -9,13 +9,7 @@
      */
     class ClearPosts {
 
-        /**
-         * Entry Point
-         */
-        public function Execute() {
-            set_time_limit(0);
-            Logger::LogLevel(ELOG_DEBUG);
-
+        private function getRemoved() {
             $sql = <<<sql
                 select a."articleId" from articles a
                 LEFT join "articleQueues" aq USING ("articleId")
@@ -32,6 +26,47 @@ sql;
 
             while ($ds->Next()) {
                 $articleIds[] = $ds->GetInteger('articleId');
+            }
+
+            return array();
+
+            //return $articleIds;
+        }
+
+        private function getPackUnused1() {
+            $sql = <<<sql
+                select a."articleId" from articles a
+                LEFT join "articleQueues" aq USING ("articleId")
+                where a."statusId" = 1 and aq."articleId" IS NULL
+                and a."sourceFeedId" IN(2, 97, 108, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 128, 130, 131, 133, 138, 140, 146, 151, 156, 157, 163, 344)
+                and "isCleaned" = false
+                order by a."articleId"
+                limit 100;
+sql;
+
+            $cmd = new SqlCommand($sql, ConnectionFactory::Get());
+            $ds = $cmd->Execute();
+
+            $articleIds = array();
+
+            while ($ds->Next()) {
+                $articleIds[] = $ds->GetInteger('articleId');
+            }
+
+            return $articleIds;
+        }
+
+        /**
+         * Entry Point
+         */
+        public function Execute() {
+            set_time_limit(0);
+            Logger::LogLevel(ELOG_DEBUG);
+
+            $articleIds = $this->getRemoved();
+
+            if (empty($articleIds)) {
+                $articleIds = $this->getPackUnused1();
             }
 
             if (empty($articleIds)) {
@@ -62,7 +97,8 @@ sql;
 
             $o = new Article();
             $o->isCleaned = true;
-            ArticleFactory::UpdateByMask($o, array('isCleaned'), array('_articleId' => $articleIds));
+            $o->statusId = 3;
+            ArticleFactory::UpdateByMask($o, array('isCleaned', 'statusId'), array('_articleId' => $articleIds));
         }
     }
 
