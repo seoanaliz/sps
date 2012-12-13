854bc12052febf826eaddb40b738bbb779cd0a88
akalie <askaslie@gmail.com>
2012-12-13 13:47:39 +0400

    добавил поиск в targetFeedIds у авторов, мелкие фиксы интерфейса

--- web/lib/SPS.Stat/StatPublics.php
+++ web/lib/SPS.Stat/StatPublics.php
@@ -31,7 +31,7 @@
                     $public->externalId ==  27421965  ||
                     $public->externalId ==  34010064  ||
                     $public->externalId ==  25749497  ||
-//                    $public->externalId ==  38000555  ||
+                    $public->externalId ==  38000555  ||
                     $public->externalId ==  35807078  ||
                     $public->externalId ==  25817269 )
                     continue;
