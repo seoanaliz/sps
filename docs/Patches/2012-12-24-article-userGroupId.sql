2009a73c1d04c263d043a293766810e8dfa3a55d
Roman Pavlenko <x100up@yandex.ru>
2012-12-25 11:27:50 +0400

    patches

---
+++ docs/Patches/2012-12-24-article-userGroupId.sql
@@ -0,0 +1,23 @@
+alter table "articles" add column "userGroupId" Int Default NULL;
+
+DROP VIEW "getArticles";
+
+CREATE OR REPLACE VIEW "getArticles" AS
+SELECT "public"."articles"."articleId"
+	, "public"."articles"."importedAt"
+	, "public"."articles"."createdAt"
+	, "public"."articles"."queuedAt"
+	, "public"."articles"."sentAt"
+	, "public"."articles"."externalId"
+	, "public"."articles"."rate"
+	, "public"."articles"."sourceFeedId"
+	, "public"."articles"."targetFeedId"
+	, "public"."articles"."authorId"
+	, "public"."articles"."editor"
+	, "public"."articles"."isCleaned"
+	, "public"."articles"."statusId"
+	, "public"."articles"."articleStatus"
+	, "public"."articles"."userGroupId"
+ FROM "public"."articles"
+	WHERE "public"."articles"."statusId" != 3
+ORDER BY "createdAt" DESC, "articleId" DESC;
