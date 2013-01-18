a40d3adde61de86b4095fe70592376369d1bc2d0
Eugene Shulepin <eugene.shulepin@gmail.com>
2013-01-18 13:19:18 +0400

    Revert "#0 "Показываем в источнках авторских пользователей с ролью автор в этом источнике""
    This reverts commit 8d7d725dc422902a3d9c572f4d3a6b23cd07d21c, reversing
    changes made to d385fb77a0c9003a233d7a7e757a5b5d9a00bb97.

--- web/lib/SPS.Site/actions/controls/GetSourceFeedsListControl.php
+++ web/lib/SPS.Site/actions/controls/GetSourceFeedsListControl.php
@@ -1,102 +1,88 @@
 <?php
-/**
- * GetSourceFeedsListControl Action
- * @package    SPS
- * @subpackage Site
- * @author     Shuler
- */
-class GetSourceFeedsListControl extends BaseControl
-{
-
     /**
-     * Entry Point
+     * GetSourceFeedsListControl Action
+     * @package    SPS
+     * @subpackage Site
+     * @author     Shuler
      */
-    public function Execute()
-    {
-        $TargetFeedAccessUtility = new TargetFeedAccessUtility($this->vkId);
+    class GetSourceFeedsListControl extends BaseControl {
 
-        $targetFeedId = Request::getInteger('targetFeedId');
+        /**
+         * Entry Point
+         */
+        public function Execute() {
+            $TargetFeedAccessUtility = new TargetFeedAccessUtility($this->vkId);
 
-        $type = Request::getString('type');
-        if (empty($type) || empty(SourceFeedUtility::$Types[$type])) {
-            $type = $TargetFeedAccessUtility->getDefaultType($targetFeedId);
-        }
+            $targetFeedId = Request::getInteger('targetFeedId');
 
-        if (!$TargetFeedAccessUtility->hasAccessToSourceType($targetFeedId, $type)) {
-            // запросили недоступный тип, но мы тогда вернем дефолтный
-            $type = $TargetFeedAccessUtility->getDefaultType($targetFeedId);
-        }
+            $type = Request::getString( 'type' );
+            if (empty($type) || empty(SourceFeedUtility::$Types[$type])) {
+                $type = $TargetFeedAccessUtility->getDefaultType($targetFeedId);
+            }
 
-        $result = array();
-        if (!empty($targetFeedId)) {
-            if ($type == SourceFeedUtility::Authors) {
-                $role = $TargetFeedAccessUtility->getRoleForTargetFeed($targetFeedId);
-                $authors = array();
-                /**
-                 * Если у юзера роль - автор - то мы ему возвращаем в источнике только себя,
-                 * Если нет - то всех авторов в этой ленте
-                 */
-                if ($role == UserFeed::ROLE_AUTHOR) {
+            if (!$TargetFeedAccessUtility->hasAccessToSourceType($targetFeedId, $type)) {
+                // запросили недоступный тип, но мы тогда вернем дефолтный
+                $type = $TargetFeedAccessUtility->getDefaultType($targetFeedId);
+            }
+
+            $result = array();
+            if (!empty($targetFeedId)) {
+                if ($type == SourceFeedUtility::Authors) {
+                    $role = $TargetFeedAccessUtility->getRoleForTargetFeed($targetFeedId);
+                    if ($role == UserFeed::ROLE_AUTHOR) {
+                        $sql = ' AND "vkId" = ' . PgSqlConvert::ToInt($this->vkId) . ' ';
+                    } else {
+                        $sql = ' AND "targetFeedIds" @> ARRAY[' . PgSqlConvert::ToInt($targetFeedId) . '] ';
+                    }
                     $authors = AuthorFactory::Get(
-                        array('vkId' => $this->vkId),
-                        array(BaseFactory::WithoutPages => true)
+                        array(),
+                        array(
+                            BaseFactory::WithoutPages => true,
+                            BaseFactory::CustomSql => $sql
+                        )
                     );
-                } else {
-                    $UserFeeds = UserFeedFactory::Get(array('targetFeedId' => $targetFeedId, 'role' => UserFeed::ROLE_AUTHOR));
-                    if ($UserFeeds){
-                        $vkIds = [];
-                        foreach ($UserFeeds as $UserFeed) {
-                            $vkIds[] = $UserFeed->vkId;
-                        }
 
-                        $authors = AuthorFactory::Get(
-                            array('vkIdIn' => $vkIds),
-                            array(BaseFactory::WithoutPages => true)
+                    foreach ($authors as $author) {
+                        $result[] =  array(
+                            'id' => $author->authorId,
+                            'title' => $author->FullName()
+                        );
+                    }
+                } else {
+                    $SourceAccessUtility = new SourceAccessUtility($this->vkId);
+
+                    $sourceIds = $SourceAccessUtility->getSourceIdsForTargetFeed($targetFeedId);
+                    $sourceFeeds = array();
+                    if ($sourceIds) {
+                        $sourceFeeds = SourceFeedFactory::Get(
+                            array(
+                                '_sourceFeedId' => $sourceIds,
+                                'type' => $type)
+                            , array(BaseFactory::WithoutPages => true)
+                        );
+                    }
+
+                    foreach ($sourceFeeds as $sourceFeed) {
+                        $result[] =  array(
+                            'id' => $sourceFeed->sourceFeedId,
+                            'title' => $sourceFeed->title
                         );
                     }
                 }
-
-                foreach ($authors as $author) {
-                    $result[] = array(
-                        'id' => $author->authorId,
-                        'title' => $author->FullName()
-                    );
-                }
             } else {
-                $SourceAccessUtility = new SourceAccessUtility($this->vkId);
-
-                $sourceIds = $SourceAccessUtility->getSourceIdsForTargetFeed($targetFeedId);
-                $sourceFeeds = array();
-                if ($sourceIds) {
-                    $sourceFeeds = SourceFeedFactory::Get(
-                        array(
-                            '_sourceFeedId' => $sourceIds,
-                            'type' => $type)
-                        , array(BaseFactory::WithoutPages => true)
-                    );
-                }
-
-                foreach ($sourceFeeds as $sourceFeed) {
-                    $result[] = array(
-                        'id' => $sourceFeed->sourceFeedId,
-                        'title' => $sourceFeed->title
-                    );
-                }
+                echo('Unknown source feed identifier');
             }
-        } else {
-            echo('Unknown source feed identifier');
+
+
+            echo ObjectHelper::ToJSON(array(
+                'type' => $type,
+                'sourceFeeds' => $result,
+                'accessibleSourceTypes' => $TargetFeedAccessUtility->getAccessibleSourceTypes($targetFeedId),
+                'canShowAuthorsList' => $TargetFeedAccessUtility->canShowAuthorList($targetFeedId),
+                'accessibleGridTypes' => array_keys($TargetFeedAccessUtility->getAccessibleGridTypes($targetFeedId)),
+                'canAddPlanCell' => $TargetFeedAccessUtility->canAddPlanCell($targetFeedId)
+            ));
         }
-
-
-        echo ObjectHelper::ToJSON(array(
-            'type' => $type,
-            'sourceFeeds' => $result,
-            'accessibleSourceTypes' => $TargetFeedAccessUtility->getAccessibleSourceTypes($targetFeedId),
-            'canShowAuthorsList' => $TargetFeedAccessUtility->canShowAuthorList($targetFeedId),
-            'accessibleGridTypes' => array_keys($TargetFeedAccessUtility->getAccessibleGridTypes($targetFeedId)),
-            'canAddPlanCell' => $TargetFeedAccessUtility->canAddPlanCell($targetFeedId)
-        ));
     }
-}
 
 ?>
