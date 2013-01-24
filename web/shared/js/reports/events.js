c8826265346ca3460beba9a53b9e4a1bc06da29f
kohver <m.arko@yandex.ru>
2013-01-24 11:20:58 +0400

    Revert "Revert "#11048 удаление и шаринг списков в отчетах""
    This reverts commit c7bbc54d4665a706b07b162f33282cadcb4f33bd.

--- web/shared/js/reports/events.js
+++ web/shared/js/reports/events.js
@@ -46,6 +46,19 @@ Control = $.extend(Control, {
         },
         get_group_list: {
             name: 'getGroupList'
+        },
+        share_list: {
+            name: 'shareGroup',
+            params: {
+                groupIds: 'groupId',
+                userIds: 'recId'
+            }
+        },
+        remove_list: {
+            name: 'deleteGroup',
+            params: {
+                groupId: 'groupId'
+            }
         }
     }
 });
