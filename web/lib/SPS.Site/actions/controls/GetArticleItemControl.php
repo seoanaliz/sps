71cf817bb0209b4530e0b137c336ba576bd9b10e
x100up <x100up@yandex.ru>
2013-01-30 10:52:18 +0400

    Добавление articleLinkPrefix в GetArticleItemControl

--- web/lib/SPS.Site/actions/controls/GetArticleItemControl.php
+++ web/lib/SPS.Site/actions/controls/GetArticleItemControl.php
@@ -1,49 +1,56 @@
 <?php
+/**
+ * GetArticleItemControl Action
+ * @package    SPS
+ * @subpackage Site
+ * @author     Shuler
+ */
+class GetArticleItemControl extends BaseControl
+{
     /**
-     * GetArticleItemControl Action
-     * @package    SPS
-     * @subpackage Site
-     * @author     Shuler
+     * Entry Point
      */
-    class GetArticleItemControl extends BaseControl {
+    public function Execute()
+    {
+        $id = Request::getInteger('id');
 
-        /**
-         * Entry Point
-         */
-        public function Execute() {
-            $id = Request::getInteger( 'id' );
-
-            if (empty($id)) {
-                return;
-            }
-
-            $object = ArticleFactory::GetById($id);
-            if (empty($object)) {
-                return;
-            }
-
-            $SourceAccessUtility = new SourceAccessUtility($this->vkId);
-
-            //check access
-            if (!$SourceAccessUtility->hasAccessToSourceFeed($object->sourceFeedId)) {
-                return;
-            }
-
-            $sourceFeed = SourceFeedFactory::GetById($object->sourceFeedId);
-            $articleRecord = ArticleRecordFactory::GetOne(array('articleId' => $object->articleId));
-
-            if (!empty($object->authorId)) {
-                $author = AuthorFactory::GetById($object->authorId);
-                Response::setParameter( 'author', $author );
-            }
-
-            Response::setParameter( 'article', $object );
-            Response::setParameter( 'articleRecord', $articleRecord );
-            Response::setParameter( 'sourceFeed', $sourceFeed );
-            Response::setArray( 'sourceInfo', SourceFeedUtility::GetInfo(array($sourceFeed)) );
-            Response::setArray( 'commentsData', CommentUtility::GetLastComments(array($object->articleId)));
-            Response::setBoolean('canEditPosts', true);
+        if (empty($id)) {
+            return;
         }
+
+        $Article = ArticleFactory::GetById($id);
+        if (empty($Article)) {
+            return;
+        }
+
+        $SourceAccessUtility = new SourceAccessUtility($this->vkId);
+
+        //check access
+        if (!$SourceAccessUtility->hasAccessToSourceFeed($Article->sourceFeedId)) {
+            return;
+        }
+
+        $sourceFeed = SourceFeedFactory::GetById($Article->sourceFeedId);
+        $articleRecord = ArticleRecordFactory::GetOne(array('articleId' => $Article->articleId));
+
+        if (!empty($Article->authorId)) {
+            $author = AuthorFactory::GetById($Article->authorId);
+            Response::setParameter('author', $author);
+        }
+
+        $articleLinkPrefix = 'http://vk.com/wall-';
+        if ($sourceFeed->type == SourceFeedUtility::Albums) {
+            $articleLinkPrefix = 'http://vk.com/photo';
+        }
+
+        Response::setParameter('article', $Article);
+        Response::setParameter('articleRecord', $articleRecord);
+        Response::setParameter('sourceFeed', $sourceFeed);
+        Response::setArray('sourceInfo', SourceFeedUtility::GetInfo(array($sourceFeed)));
+        Response::setArray('commentsData', CommentUtility::GetLastComments(array($Article->articleId)));
+        Response::setBoolean('canEditPosts', true);
+        Response::setString('articleLinkPrefix', $articleLinkPrefix);
     }
+}
 
 ?>
