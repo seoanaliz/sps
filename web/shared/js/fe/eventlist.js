e30e7d338d9d8e59b9d933c9acc8bc1dc311abc7
kohver <m.arko@yandex.ru>
2013-01-30 17:11:43 +0400

    Свитчер на сайте

--- web/shared/js/fe/eventlist.js
+++ web/shared/js/fe/eventlist.js
@@ -6,10 +6,7 @@ Control = $.extend(Control, {
 
     controlMap: {
         get_articles: {
-            name: 'arcticles-list',
-            params: {
-                articlesOnly: 'articles-only'
-            }
+            name: 'arcticles-list'
         },
         authors_get: {
             name: 'authors-list'
