ac5c4cd0e55836e3bae7800578a817264bfabc64
x100up <x100up@yandex.ru>
2013-02-19 15:42:45 +0400

    Рефаторииг контрола добавления поста в очередь

--- web/lib/SPS.Site/actions/controls/AddArticleToQueueControl.php
+++ web/lib/SPS.Site/actions/controls/AddArticleToQueueControl.php
@@ -1,147 +1,156 @@
 <?php
+/**
+ * AddArticleToQueueControl Action
+ * @package    SPS
+ * @subpackage Site
+ * @author     Shuler
+ */
+class AddArticleToQueueControl extends BaseControl
+{
+
     /**
-     * AddArticleToQueueControl Action
-     * @package    SPS
-     * @subpackage Site
-     * @author     Shuler
+     * Entry Point
      */
-    class AddArticleToQueueControl extends BaseControl {
+    public function Execute()
+    {
+        $result = array(
+            'success' => false
+        );
 
-        /**
-         * Entry Point
-         */
-        public function Execute() {
+        $articleId = Request::getInteger('articleId');
+        $targetFeedId = Request::getInteger('targetFeedId');
+        $timestamp = Request::getInteger('timestamp');
+        $queueId = Request::getInteger('queueId');
+        $type = Request::getString('type');
 
+        if (empty($articleId) || empty($targetFeedId) || empty($timestamp) || empty($type) || empty(GridLineUtility::$Types[$type])) {
+            $result['message'] = 'Wrong data';
+            echo ObjectHelper::ToJSON($result);
+            return false;
+        }
+
+        if (!empty($queueId)) {
+            //просто перемещаем элемент очереди
+            ArticleUtility::ChangeQueueDates($queueId, $timestamp);
 
             $result = array(
-                'success' => false
+                'success' => true,
+                'id' => $queueId
+            );
+            echo ObjectHelper::ToJSON($result);
+            return true;
+        }
+
+        // может ли планировать в ленту
+        $TargetFeedAccessUtility = new TargetFeedAccessUtility($this->vkId);
+        if (!$TargetFeedAccessUtility->canAddArticlesQueue($targetFeedId)) {
+            $result['message'] = 'AccessDenied!';
+            echo ObjectHelper::ToJSON($result);
+            return false;
+        }
+
+        // получаем пост
+        $article = ArticleFactory::GetById($articleId);
+        if (!$article) {
+            $result['message'] = 'ArticleNotFound:' . $articleId;
+            echo ObjectHelper::ToJSON($result);
+            return false;
+        }
+
+
+        $targetFeed = TargetFeedFactory::GetById($targetFeedId);
+        $articleRecord = ArticleRecordFactory::GetOne(array('articleId' => $articleId));
+
+        if (empty($targetFeed)) {
+            $result['message'] = 'Empty Target Feed';
+            echo ObjectHelper::ToJSON($result);
+            return false;
+        }
+
+        if (empty($articleRecord)) {
+            $result['message'] = 'Empty Article Record';
+            echo ObjectHelper::ToJSON($result);
+            return false;
+        }
+
+
+        //source feed
+        $sourceFeed = SourceFeedFactory::GetById($article->sourceFeedId);
+        $sourceFeed = !empty($sourceFeed) ? $sourceFeed : new SourceFeed();
+
+        if ($sourceFeed->type != SourceFeedUtility::Ads) {
+            //проверяем, если ли такая $articleId в этой $targetFeedId
+            $existsCount = ArticleQueueFactory::Count(
+                array('articleId' => $articleId, 'targetFeedId' => $targetFeedId)
             );
 
-            $articleId = Request::getInteger( 'articleId' );
-            $targetFeedId = Request::getInteger( 'targetFeedId' );
-            $timestamp = Request::getInteger( 'timestamp' );
-            $queueId = Request::getInteger( 'queueId' );
-            $type = Request::getString('type');
-
-            if (empty($articleId) || empty($targetFeedId) || empty($timestamp) || empty($type) || empty(GridLineUtility::$Types[$type])) {
+            if ($existsCount) {
+                $result['message'] = 'articleQueueExists';
                 echo ObjectHelper::ToJSON($result);
                 return false;
             }
+        }
 
-            if (!empty($queueId)) {
-                //просто перемещаем элемент очереди
-                ArticleUtility::ChangeQueueDates($queueId, $timestamp);
+        // добавляем улемент очереди
+        $ArticleQueue = new ArticleQueue();
+        $ArticleQueue->createdAt = DateTimeWrapper::Now();
+        $ArticleQueue->articleId = $article->articleId;
+        $ArticleQueue->targetFeedId = $targetFeed->targetFeedId;
+        $ArticleQueue->type = $type;
+        $ArticleQueue->author = AuthVkontakte::IsAuth();
+        ArticleUtility::BuildDates($ArticleQueue, $timestamp);
+        $ArticleQueue->isDeleted = false;
+        $ArticleQueue->collectLikes = true;
+        $ArticleQueue->deleteAt = null;
+        $ArticleQueue->statusId = 1;
 
-                $result = array(
-                    'success' => true,
-                    'id' => $queueId
-                );
-                echo ObjectHelper::ToJSON($result);
-                return true;
+        $articleQueueRecord = clone $articleRecord;
+        $articleQueueRecord->articleRecordId = null;
+        $articleQueueRecord->articleId = null;
+
+        ConnectionFactory::BeginTransaction();
+
+        $sqlResult = ArticleQueueFactory::Add($ArticleQueue);
+
+        if ($sqlResult) {
+            $articleQueueRecord->articleQueueId = ArticleQueueFactory::GetCurrentId();
+
+            $sqlResult = ArticleRecordFactory::Add($articleQueueRecord);
+        }
+
+        ConnectionFactory::CommitTransaction($sqlResult);
+
+        if ($sqlResult) {
+            if (!empty($article->authorId)) {
+                //author event
+                AuthorEventUtility::EventQueue($article);
             }
 
-            $article = ArticleFactory::GetById($articleId);
+            AuditUtility::CreateEvent(
+                'articleQueue'
+                , 'article'
+                , $article->articleId
+                , "Queued by editor VkId " . AuthUtility::GetCurrentUser('Editor')->vkId . ", queueId is " . $articleQueueRecord->articleQueueId);
 
-            if (!$article){
-                $result['message'] = 'ArticleNotFound:' . $articleId;
-                $result['art'] = print_r($article, true);
-                echo ObjectHelper::ToJSON($result);
-                return false;
-            }
-
-            //check access
-            $TargetFeedAccessUtility = new TargetFeedAccessUtility($this->vkId);
-            $SourceAccessUtility = new SourceAccessUtility($this->vkId);
-            if (!$TargetFeedAccessUtility->canAddArticlesQueue($targetFeedId)
-                || !$SourceAccessUtility->hasAccessToSourceFeed($article->sourceFeedId)) {
-                echo ObjectHelper::ToJSON(array('success' => false));
-                return false;
-            }
-
-            $targetFeed = TargetFeedFactory::GetById($targetFeedId);
-            $articleRecord = ArticleRecordFactory::GetOne(array('articleId' => $articleId));
-
-            if (empty($article) || empty($targetFeed) || empty($articleRecord)) {
-                echo ObjectHelper::ToJSON($result);
-                return false;
-            }
-
-            //source feed
-            $sourceFeed = SourceFeedFactory::GetById($article->sourceFeedId);
-            $sourceFeed = !empty($sourceFeed) ? $sourceFeed : new SourceFeed();
+            $result = array(
+                'success' => true,
+                'id' => $articleQueueRecord->articleQueueId
+            );
 
             if ($sourceFeed->type != SourceFeedUtility::Ads) {
-                //проверяем, если ли такая $articleId в этой $targetFeedId
-                $existsCount = ArticleQueueFactory::Count(
-                    array('articleId' => $articleId, 'targetFeedId' => $targetFeedId)
-                );
+                //блокируем статью, чтобы ее больше никто не пытался отправить
+                $o = new Article();
+                $o->statusId = 2;
+                $o->queuedAt = DateTimeWrapper::Now();
+                ArticleFactory::UpdateByMask($o, array('statusId', 'queuedAt'), array('articleId' => $article->articleId));
 
-                if ($existsCount) {
-                    $result['message'] = 'articleQueueExists';
-                    echo ObjectHelper::ToJSON($result);
-                    return false;
-                }
+                $result['moved'] = true;
+            } else {
+                $result['moved'] = false;
             }
-
-            $object = new ArticleQueue();
-            $object->createdAt = DateTimeWrapper::Now();
-            $object->articleId = $article->articleId;
-            $object->targetFeedId = $targetFeed->targetFeedId;
-            $object->type = $type;
-            $object->author = AuthVkontakte::IsAuth();
-            ArticleUtility::BuildDates($object, $timestamp);
-            $object->isDeleted = false;
-            $object->collectLikes = true;
-            $object->deleteAt = null;
-
-            $object->statusId = 1;
-
-            $articleQueueRecord = clone $articleRecord;
-            $articleQueueRecord->articleRecordId = null;
-            $articleQueueRecord->articleId = null;
-
-            ConnectionFactory::BeginTransaction();
-            
-            $sqlResult = ArticleQueueFactory::Add($object);
-
-            if ($sqlResult) {
-                $articleQueueRecord->articleQueueId = ArticleQueueFactory::GetCurrentId();
-                
-                $sqlResult = ArticleRecordFactory::Add($articleQueueRecord);
-            }
-
-            ConnectionFactory::CommitTransaction($sqlResult);
-
-            if ($sqlResult) {
-                if (!empty($article->authorId)) {
-                    //author event
-                    AuthorEventUtility::EventQueue($article);
-                }
-
-                AuditUtility::CreateEvent(
-                    'articleQueue'
-                    , 'article'
-                    , $article->articleId
-                    , "Queued by editor VkId " . AuthUtility::GetCurrentUser('Editor')->vkId . ", queueId is " . $articleQueueRecord->articleQueueId);
-
-                $result = array(
-                    'success' => true,
-                    'id' => $articleQueueRecord->articleQueueId
-                );
-
-                if ($sourceFeed->type != SourceFeedUtility::Ads) {
-                    //блокируем статью, чтобы ее больше никто не пытался отправить
-                    $o = new Article();
-                    $o->statusId = 2;
-                    $o->queuedAt = DateTimeWrapper::Now();
-                    ArticleFactory::UpdateByMask($o, array('statusId', 'queuedAt'), array('articleId' => $article->articleId));
-
-                    $result['moved'] = true;
-                } else {
-                    $result['moved'] = false;
-                }
-            }
-            echo ObjectHelper::ToJSON($result);
         }
+        echo ObjectHelper::ToJSON($result);
     }
+}
+
 ?>
