a37084f9829e5f1cf6e3279fe55f059d674f75c3
akalie <askaslie@gmail.com>
2013-02-20 15:19:52 +0400

    fixes

--- web/lib/SPS.Stat/actions/daemons/TokenChecker.php
+++ web/lib/SPS.Stat/actions/daemons/TokenChecker.php
@@ -10,11 +10,13 @@ class TokenChecker
 
     const ALERT_TOKEN = "9a52c2c5ad3c3a0dba10d682cd5e70e99aea7ca665701c2f754fb94e33775cf842485db7b5ec5fb49b2d5";
     public function execute() {
-        $publishers = PublisherFactory::Get();
+        $publishers = PublisherFactory::Get(array('status_id' => 1));
         $errors = 0;
         foreach( $publishers as $publisher ) {
             $res = VkHelper::get_vk_time( $publisher->vk_token );
             if( isset( $res->error )) {
+                print_r($res->error->error_msg);
+                echo '<br>' . $publisher->vk_id . '<br><br>';
                 AuditUtility::CreateEvent(
                       'accessTokenDead'
                     , 'publisher'
@@ -22,18 +24,19 @@ class TokenChecker
                     , $publisher->name . " ( " . $publisher->vk_id . " )" . " was presumably banned ");
                 $errors ++;
             }
-            sleep(0.5);
+            sleep(1);
         }
-
 
         if ( $errors ) $this->send_alert();
 
     }
 
+
+
     private function send_alert() {
         $params = array(
             'uid'           =>   670456,
-            'message'       =>  'rise and shine! tokens are dead.',
+            'message'       =>  'rise and shine! tokens are dead. ' . md5(time()) ,
             'access_token'  =>  self::ALERT_TOKEN,
         );
         VkHelper::api_request( 'messages.send', $params );
