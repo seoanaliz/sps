d0e4b0b66bb8148286130aff146291f09792a334
Eugene Shulepin <eugene.shulepin@gmail.com>
2013-03-07 10:37:05 +0400

    #00 временно отключаем компиляцию пакетов

--- web/eaze.production.php
+++ web/eaze.production.php
@@ -1,7 +1,7 @@
 <?php
     /* Don't Forget to turn on mod_rewrite!  */
 
-    define( 'WITH_PACKAGE_COMPILE', false  );
+    define( 'WITH_PACKAGE_COMPILE', true  );
 
     include_once 'lib/Eaze.Core/Logger.php';
     // Initialize Logger
