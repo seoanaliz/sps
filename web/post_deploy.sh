bbdc106e00767f59dedbc18f764b18c960506ff3
Eugene Shulepin <eugene.shulepin@gmail.com>
2013-03-07 09:38:02 +0400

    #00 новый конфиг прода

--- web/post_deploy.sh
+++ web/post_deploy.sh
@@ -14,6 +14,7 @@ echo -e "\t $1/shared/temp/ to 777"
 chmod -f -R 777 $1/shared/temp/
 
 echo -e "\t Production Version"
+rm $1cache/*
 rm $1cache/compiled.eaze
 rm $1/eaze.php
 mv $1/eaze.production.php $1/eaze.php
