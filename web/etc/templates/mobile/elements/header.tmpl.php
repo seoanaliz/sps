3926c3862ed930a01846f89ca6f017741c429883
kohver <m.arko@yandex.ru>
2013-03-15 20:39:27 +0400

    #13271 Бесконечный скроллинг в ленте отправки.

---
+++ web/etc/templates/mobile/elements/header.tmpl.php
@@ -0,0 +1,72 @@
+<?
+if (!isset($__activeElement)) $__activeElement = NULL;
+
+/**
+ * Manual set meta or reset of meta
+ */
+$__sitePageTitle    = 'Ваше сообщество в App Store и Google Play';
+$__pageTitle        = !empty($__pageTitle) ? $__pageTitle : '';
+$__metaDescription  = !empty($__metaDescription) ? $__metaDescription : '';
+$__metaKeywords     = !empty($__metaKeywords) ? $__metaKeywords : '';
+$__imageAlt         = !empty($__imageAlt) ? $__imageAlt : '';
+
+/*
+* Meta tags from MetaDetail object or Page object
+*/
+if (!empty($__metaDetail)) {
+    if (!empty($__metaDetail->pageTitle))       $__pageTitle       = $__metaDetail->pageTitle;
+    if (!empty($__metaDetail->metaDescription)) $__metaDescription = $__metaDetail->metaDescription;
+    if (!empty($__metaDetail->metaKeywords))    $__metaKeywords    = $__metaDetail->metaKeywords;
+    if (!empty($__metaDetail->alt))         	$__imageAlt        = $__metaDetail->alt;
+} else if(!empty($__page)) {
+    $__pageTitle = !empty($__page->pageTitle) ? $__page->pageTitle : ($__page->title . ' | ' . $__sitePageTitle);
+
+    if (!empty($__page->metaDescription))       $__metaDescription = $__page->metaDescription;
+    if (!empty($__page->metaKeywords))          $__metaKeywords    = $__page->metaKeywords;
+}
+
+/**
+ * Default page title
+ */
+$__pageTitle = !empty($__pageTitle) ? $__pageTitle : $__sitePageTitle;
+
+$cssFiles = array(
+    AssetHelper::AnyBrowser => array(
+        'css://common/common.css',
+        'css://mobile/mobile.css',
+   ),
+    AssetHelper::IE7 => array(),
+);
+
+$jsFiles = array(
+    'js://common/jquery-1.7.2.min.js',
+    'js://common/common.js',
+    'js://mobile/mobile.js',
+);
+
+CssHelper::Init(false);
+JsHelper::Init(true);
+
+CssHelper::PushGroups($cssFiles);
+if(!empty($cssFilesAdds)) {
+    CssHelper::PushGroups($cssFilesAdds);
+}
+
+JsHelper::PushFiles($jsFiles);
+if(!empty($jsFilesAdds)) {
+    JsHelper::PushFiles($jsFilesAdds);
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?= LocaleLoader::$HtmlEncoding ?>" />
+    <title><?=$__pageTitle?></title>
+    <meta name="keywords" content="{form:$__metaKeywords}" />
+    <meta name="description" content="{form:$__metaDescription}" />
+    <link rel="icon" href="{web:/favicon.ico}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{web:/favicon.ico}" type="image/x-icon" />
+    <?= CssHelper::Flush(); ?>
+    <?= JsHelper::Flush(); ?>
+</head>
+<body>
--- web/etc/templates/mobile/index.tmpl.php
+++ web/etc/templates/mobile/index.tmpl.php
@@ -1,10 +1,58 @@
-<!doctype html>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
-Hello!
-</body>
-</html>
+{increal:tmpl://mobile/elements/header.tmpl.php}
+<div id="main" class="main">
+    <div id="header">
+        <div class="inner-content block-wrapper-2">
+            <div class="block">Logo</div>
+            <div class="block right">Widget</div>
+        </div>
+    </div>
+    <div id="main-content">
+        <div class="inner-content block-wrapper-2">
+            <div class="block">
+                <div class="block">
+                    Ваше сообщество в App Store и Google Play
+                </div>
+                <div class="block">
+                    <button class="big-button">Создать приложение</button>
+                </div>
+                <div class="block">
+                    Необходима авторизация VK
+                </div>
+            </div>
+            <div class="block">
+                <div id="mobiles"></div>
+            </div>
+        </div>
+    </div>
+    <div id="description">
+        <div class="inner-content block-wrapper-3">
+            <div class="block">
+                <div class="reason">
+                    <div class="icon"></div>
+                    <div class="title">Simple</div>
+                    <div class="description">ssssssssss</div>
+                </div>
+            </div>
+            <div class="block">
+                <div class="reason">
+                    <div class="icon"></div>
+                    <div class="title">Simple</div>
+                    <div class="description">ssssssssss</div>
+                </div>
+            </div>
+            <div class="block">
+                <div class="reason">
+                    <div class="icon"></div>
+                    <div class="title">Simple</div>
+                    <div class="description">ssssssssss</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <div class="inner-content">
+            &copy; SocialBoard 2013
+        </div>
+    </div>
+</div>
+{increal:tmpl://mobile/elements/footer.tmpl.php}
