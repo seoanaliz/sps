34e82b3c92c65a40f1f030361f016192919904cb
akalie <askaslie@gmail.com>
2013-04-23 13:42:21 +0400

    пока убрал шапку в с стате(осталась у редакотров)

--- web/shared/js/st/template.js
+++ web/shared/js/st/template.js
@@ -1,13 +1,14 @@
 var LIST =
-'<div class="tab-bar clear-fix">' +
-    '<div class="tab selected" data-id="null">Популярные</div>' +
-    '<? each(LIST_ITEM, items); ?>' +
-    '<div class="actions">' +
-        '<a class="share">Поделиться</a> |' +
-        '<a class="edit">Редактировать</a> |' +
-        '<a class="delete">Удалить</a>' +
-    '</div> ' +
-'</div>';
+'<?if( cur.dataUser.isEditor ) { ?>'+
+    '<div class="tab-bar clear-fix">' +
+        '<div class="actions">' +
+            '<a class="share">Поделиться</a> |' +
+            '<a class="edit">Редактировать</a> |' +
+            '<a class="delete">Удалить</a>' +
+        '</div> ' +
+    '</div>' +
+' <? } ?> ';
+
 
 var LIST_ITEM =
 '<? if (isset("itemFave")) { ?>' +
