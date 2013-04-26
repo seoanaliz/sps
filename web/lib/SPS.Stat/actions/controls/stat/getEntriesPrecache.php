2ea7034e171eac4b5c27674b7e8e96caac9b55de
Evgeny Kulikov <beyondcompute@gmail.com>
2013-04-26 12:00:42 +0400

    created pseudo-model, factored out db access code from controller

---
+++ web/lib/SPS.Stat/actions/controls/stat/getEntriesPrecache.php
@@ -0,0 +1,19 @@
+<?php
+    class getEntriesPrecache {
+
+        /**
+         * Entry Point
+         */
+        public function Execute() {
+            $requestData = Page::$RequestData;
+            $slug = isset($requestData[1]) ? $requestData[1] : null;
+            $EntryGetter = new EntryGetter();
+            $id = null;
+            if ($slug) {
+                $id = $EntryGetter->getGroupIdBySlug($slug);
+            }
+            Request::setInteger('groupId', $id); // Нужно, т.к. EntryGetter зависит от глобального состояния (Request)
+            Response::setString( 'entriesPrecache', ObjectHelper::ToJSON($EntryGetter->getEntriesData()) );
+        }
+    }
+?>
