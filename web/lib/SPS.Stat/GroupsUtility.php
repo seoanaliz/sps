e6d5ceadbbf0674ad08799d593f9d3204e8b9333
akalie <askaslie@gmail.com>
2013-04-29 19:21:16 +0400

    новый тип списков в обменах

--- web/lib/SPS.Stat/GroupsUtility.php
+++ web/lib/SPS.Stat/GroupsUtility.php
@@ -5,6 +5,10 @@
     class GroupsUtility
     {
 
+        const Group_Type_Default = 1;
+        const Group_Shared = 2;
+        const Group_Shared_Special = 3;
+
         public static  $barter_watchers = array(
             '670456',
             '106175502',
@@ -126,7 +130,7 @@
         }
 
         //формирует отчет для групп. Если указан user_id, разделяет созданные им группы и нет
-        public static function form_response( $groups, $user_id, $group_source )
+        public static function form_response(  $groups, $user_id, $group_source )
         {
         //todo place
             if( !is_array( $groups ))
@@ -135,17 +139,17 @@
             $i = 1;
 
             foreach( $groups as $group ) {
-                if( $group->created_by != $user_id ) {
+                /** @var $group Group*/
+                if( $group->created_by != $user_id && $group->type != self::Group_Shared_Special ) {
                     $user_shared_groups[$group->created_by][] = $group->group_id;
                 } else {
-
                     $tmp = array(
                         'group_id'  =>  $group->group_id,
                         'type'      =>  $group->type,
                         'name'      =>  $group->type == 2 ? 'Мой первый список' : $group->name,
                         'place'     =>  $group->type == 2 ? 0 : $i++
                     );
-                    if( $group->type == 2 ) {
+                    if( $group->type == 2 || $group->type == 3 ) {
                         if( !is_array( $res['user_lists']))
                             $res['user_lists'] = array();
                         array_unshift( $res['user_lists'], $tmp );
--- web/lib/SPS.Stat/actions/controls/groups/getGroupList.php
+++ web/lib/SPS.Stat/actions/controls/groups/getGroupList.php
@@ -29,6 +29,7 @@
             }
             if ( $type == 'Barter' ) {
                 $source = 1;
+                GroupsUtility::get_default_group( $user_id, Group::BARTER_GROUP );
                 $groups = GroupFactory::Get( array( 'status' => 1, 'source' => Group::BARTER_GROUP, '_users_ids' => array( $user_id )));
                 die( ObjectHelper::ToJSON( array( 'response' => GroupsUtility::form_response( $groups, $user_id, Group::BARTER_GROUP ))));
             }
