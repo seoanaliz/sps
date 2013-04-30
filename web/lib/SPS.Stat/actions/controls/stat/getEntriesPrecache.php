5f6419e4d63ff5ed9c749ae2ade2298de5cdc968
Evgeny Kulikov <beyondcompute@gmail.com>
2013-04-30 15:59:47 +0400

    hack for updating slugs

--- web/lib/SPS.Stat/actions/controls/stat/getEntriesPrecache.php
+++ web/lib/SPS.Stat/actions/controls/stat/getEntriesPrecache.php
@@ -7,9 +7,14 @@
         public function Execute() {
             $requestData = Page::$RequestData;
             $slug = isset($requestData[1]) ? $requestData[1] : null;
+            
             $EntryGetter = new EntryGetter();
             $id = null;
             if ($slug) {
+                if ($slug === "~update") {
+                    $EntryGetter->updateSlugs();
+                    die('done');
+                }
                 $id = $EntryGetter->getGroupIdBySlug($slug);
             }
             Request::setInteger('groupId', $id); // Нужно, т.к. EntryGetter зависит от глобального состояния (Request)
