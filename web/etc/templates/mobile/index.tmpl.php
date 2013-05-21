18b5c72c02cfb3ba43e4f08ac32c03169e4fe766
Evgeny Kulikov <beyondcompute@gmail.com>
2013-05-21 12:54:25 +0400

    all

--- web/etc/templates/mobile/index.tmpl.php
+++ web/etc/templates/mobile/index.tmpl.php
@@ -1,33 +1,31 @@
 {increal:tmpl://mobile/elements/header.tmpl.php}
-<div class="topmost">
+<div class="topmost col">
     <div class="wrap">
         <a href="javascript:;" class="create-app"></a>
     </div>
 </div>
-<div class="gradient-stripe">
-    <div class="wrap">
-        &nbsp;
-    </div>
-</div>
-<div class="n1">
+<div class="gradient-stripe col">
     &nbsp;
 </div>
-<div class="n2">
+<div class="n1 col">
     &nbsp;
 </div>
-<div class="n3">
+<div class="n2 col">
     &nbsp;
 </div>
-<div class="n4">
+<div class="n3 col">
     &nbsp;
 </div>
-<div class="n5">
+<div class="n4 col">
     &nbsp;
 </div>
-<div class="green">
+<div class="n5 col">
     &nbsp;
 </div>
-<div class="footer-white">
+<div class="green col">
+    &nbsp;
+</div>
+<div class="footer-white col">
     <div class="wrap">
         <a href="http://www.inner-active.com" target="_blank" class="inneractive"></a>
         <a href="http://www.google.com/ads/admob/" target="_blank" class="admob"></a>
@@ -35,7 +33,7 @@
         <a href="http://www.adwired.net" target="_blank" class="adwired"></a>
     </div>
 </div>
-<div class="footer-gray">
+<div class="footer-gray col">
     <div class="wrap">
         <a href="javascript:;" class="create-app"></a>
     </div>
