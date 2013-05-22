842aae0e679f74eca03438a05a758a356f3dd99d
Evgeny Kulikov <beyondcompute@gmail.com>
2013-05-22 19:32:43 +0400

    all

--- web/etc/templates/mobile/index.tmpl.php
+++ web/etc/templates/mobile/index.tmpl.php
@@ -1,41 +1,82 @@
 {increal:tmpl://mobile/elements/header.tmpl.php}
-<div class="topmost col">
+<div class="header">
     <div class="wrap">
-        <a href="javascript:;" class="create-app"></a>
+        <h1>Socialboard</h1>
+        <h2>Мобильное приложение<br />для вашего сообщества</h2>
+        <button class="create-app">Создать бесплатно</button>
+        <h4 class="ready">готовое решение</h4>
+        <h4 class="only14">всего за 14 дней</h4>
+        <div class="phone-wrap"><img alt="" src="shared/images/mobile/header-phone-screen.jpg" /></div>
     </div>
 </div>
-<div class="gradient-stripe col">
-    &nbsp;
-</div>
-<div class="n1 col">
-    &nbsp;
-</div>
-<div class="n2 col">
-    &nbsp;
-</div>
-<div class="n3 col">
-    &nbsp;
-</div>
-<div class="n4 col">
-    &nbsp;
-</div>
-<div class="n5 col">
-    &nbsp;
-</div>
-<div class="green col">
-    &nbsp;
-</div>
-<div class="footer-white col">
+<div class="available-in">
     <div class="wrap">
-        <a href="http://www.inner-active.com" target="_blank" class="inneractive"></a>
-        <a href="http://www.google.com/ads/admob/" target="_blank" class="admob"></a>
-        <a href="http://www.mopub.com" target="_blank" class="mopub"></a>
-        <a href="http://www.adwired.net" target="_blank" class="adwired"></a>
+        <h2 class="appstore">Доступно в AppStore</h2>
+        <h2 class="googleplay">Доступно в Google Play</h2>
     </div>
 </div>
-<div class="footer-gray col">
+<div class="n1">
     <div class="wrap">
-        <a href="javascript:;" class="create-app"></a>
+        <h2>Лента новостей</h2>
+        <p>Делитесь самым интересным контентом <br />
+            с владельцами смартфонов, формируя вокруг <br />
+            своего сообщества аудиторию не зависимую <br />
+            от VK.
+        </p>
+        <div class="phone-wrap"><img alt="" src="shared/images/mobile/n1-screen.jpg" /></div>
+    </div>
+</div>
+<div class="n2">
+    <div class="wrap">
+        <h2>Комментарии</h2>
+        <p>Призывайте к обсуждению — это очень <br />
+            хорошо отразится на вашей посещаемости и <br />
+            всех других показателях. Регулярно <br />
+            отвечайте на комментарии и задавайте <br />
+            вопросы.
+        </p>
+        <div class="phone-wrap"></div>
+    </div>
+</div>
+<div class="n3">
+    <div class="wrap">
+        <h2>Меню</h2>
+        <p>Структурируйте контент, позволив найти <br />
+            вашим пользователям именно то, что им <br />
+            интересно — в любое время.
+        </p>
+        <div class="phone-wrap"></div>
+    </div>
+</div>
+<div class="n4">
+    <div class="wrap">
+        <h2>Push-уведомления</h2>
+        <p>Отправляйте уведомления на главный экран <br />
+            телефона всем, кто установил ваше <br />
+            приложения. Сообщайте о самых интересных <br />
+            публикациях в ленте, напоминая о своем <br />
+            приложении.
+        </p>
+        <div class="phone-wrap"><img alt="" src="shared/images/mobile/n4-screen.jpg" /></div>
+    </div>
+</div>
+<div class="n5">
+    <div class="wrap">
+        <h2>Обмен установками</h2>
+        <p>Сотрудничайте с владельцами других <br />
+            мобильных сообществ, чтобы значительно <br />
+            ускорить рост своей аудитории. Для этого мы <br />
+            реализовали специальный тип публикаций с <br />
+            кнопкой «Установить». Задать необходимое <br />
+            количество показов и содержание таких <br />
+            публикаций можно в панели управления.
+        </p>
+        <div class="phone-wrap"></div>
+    </div>
+</div>
+<div class="green-footer">
+    <div class="wrap">
+        <div class="phone-wrap"></div>
     </div>
 </div>
 {increal:tmpl://mobile/elements/footer.tmpl.php}
