41a736882cee7fd846b01cab18ab287f6ee95810
Evgeny Kulikov <beyondcompute@gmail.com>
2013-05-29 09:01:46 +0400

    mrg ie 8 datetime fix

--- web/shared/js/st/main.js
+++ web/shared/js/st/main.js
@@ -488,7 +488,7 @@ var Filter = (function() {
                     Math.round(dateTo ? (dateTo.getTime() / 1000) : null)
                 ]);
             });
-            $timeTo.datepicker('setDate', new Date(Date.now() - TIME.DAY));
+            $timeTo.datepicker('setDate', new Date((new Date).getTime() - TIME.DAY));
             $timeFrom.datepicker('setDate', new Date($timeTo.datepicker('getDate').getTime() - TIME.DAY));
             var dateFrom = $timeFrom.datepicker('getDate');
             var dateTo = $timeTo.datepicker('getDate');
