4d26f950dfcf4816f28a24f3acd2f4835ff83714
Evgeny Kulikov <beyondcompute@gmail.com>
2013-06-07 10:16:23 +0400

    stuff

--- web/lib/SPS.VK/actions/VKLoginRedirect.php
+++ web/lib/SPS.VK/actions/VKLoginRedirect.php
@@ -24,6 +24,44 @@
                     $array=true
                 );
                 if (isset($answer['user_id'])) {
+                    if (isset($answer['access_token'])) {
+                        $code = 'return {"permissions": API.getUserSettings(), "publics": API.groups.get({filter: "admin"})};';
+
+                        $wasError = false;
+                        try {
+                            $apiAnswer = VkHelper::api_request(
+                                    'execute',
+                                    array(
+                                        'uid' => $answer['user_id'],
+                                        'access_token' => $answer['access_token'],
+                                        'code' => $code
+                                    ));
+                        } catch (Exception $E) {
+                            $wasError = true;
+                            Logger::Error('login VK API error: ' . $E->getMessage());
+                        }
+                        if (!$wasError) {
+                            // object(stdClass)#228 (2) { ["permissions"]=> int(1376256) ["publics"]=> array(1) { [0]=> int(27421965) } } ﻿
+                            if (property_exists($apiAnswer, 'permissions') && property_exists($apiAnswer, 'publics')) {
+                                if (($apiAnswer->permissions & VkHelper::PERM_GROUPS) &&
+                                    ($apiAnswer->permissions & VkHelper::PERM_GROUP_STATS) &&
+                                    ($apiAnswer->permissions & VkHelper::PERM_OFFLINE)
+                                ) {
+                                    $accessToken = new AccessToken();
+                                    $accessToken->vkId = $answer['user_id'];
+                                    $accessToken->accessToken = $answer['access_token'];
+                                    $accessToken->appId = AuthVkontakte::$AppId;
+                                    $accessToken->createdAt = DateTimeWrapper::Now();
+                                    $accessToken->statusId  = StatusUtility::Enabled;
+                                    AccessTokenFactory::Add($accessToken);
+                                    
+                                    
+                                } else {
+                                    Logger::Error('login permissions problem for user: ' . $answer['user_id'] . ' - permissions are: ' . $apiAnswer->permissions . ' instead of: ' . (VkHelper::PERM_GROUPS + VkHelper::PERM_GROUP_STATS + VkHelper::PERM_OFFLINE));
+                                }
+                            }
+                        }
+                    }
                     AuthVkontakte::Logout();
                     AuthVkontakte::Login($answer['user_id']);
                 }
