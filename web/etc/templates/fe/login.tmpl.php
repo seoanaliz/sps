6f7860f1e0caf2f83d45c22032cf7e27347f0329
Evgeny Kulikov <beyondcompute@gmail.com>
2013-07-04 13:54:43 +0400

    fix css helper bug

--- web/etc/templates/fe/login.tmpl.php
+++ web/etc/templates/fe/login.tmpl.php
@@ -2,11 +2,6 @@
     CssHelper::PushFile('css://common/common.less');
     CssHelper::PushFile('css://fe/login.css');
     CssHelper::Init( true );
-
-    CssHelper::PushGroups( $cssFiles );
-    if( !empty( $cssFilesAdds ) ) {
-        CssHelper::PushGroups( $cssFilesAdds );
-    }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
