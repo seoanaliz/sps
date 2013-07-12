a05d341b9a418a737bc2e9e41c919f75996cc133
Evgeny Kulikov <beyondcompute@gmail.com>
2013-07-12 19:23:15 +0300

    removed commented code

--- web/shared/js/st/main.js
+++ web/shared/js/st/main.js
@@ -52,38 +52,6 @@ function checkVkStatus() {
     }
 }
 
-// taken from http://www.quirksmode.org/js/cookies.html
-//functioncreateCookie(name, value, days) {
-            //    if (days) {
-//        var date = new Date();
-//        date.setTime(date.getTime()+(days*24*60*60*1000));
-//        var expires = "; expires="+date.toGMTString();
-//    }
-//    else {
-//        expires = "";
-//    }
-//    document.cookie = name+"="+value+expires+"; path=/";
-//}
-//
-//functionreadCookie(name) {
-//    var nameEQ = name + "=";
-                //    var ca = document.cookie.split(';');
-//    for (var i=0; i < ca.length; i++) {
-//        var c = ca[i];
-//        while (c.charAt(0)==' ') {
-//            c = c.substring(1,c.length);
-//        }
-//        if (c.indexOf(nameEQ) == 0) {
-//            return c.substring(nameEQ.length,c.length);
-//        }
-//    }
-//    return null;
-//}
-//
-//function removeCookie(name) {
-//    createCookie(name, "", -1);
-//}
-
 function authInfo(response) {
     if (!response.session) {
         makeVkButton();
