ba54f568b90cb65b29204d271363e93b4e9f24bc
Evgeny Kulikov <beyondcompute@gmail.com>
2013-07-16 14:07:04 +0400

    rm second ".tab-bar" element

--- web/shared/js/st/template.js
+++ web/shared/js/st/template.js
@@ -1,17 +1,13 @@
-var LIST =
-'<div class="tab-bar clear-fix">' +
-    '<div class="tab selected" data-id="null">Популярные</div>' +
-    '<?if (cur.dataUser.isEditor) { ?>'+
-        '<div class="actions">' +
-            '<a class="share">Поделиться</a>' +
-            '<a class="edit">Редактировать</a>' +
-            '<?if (cur.dataUser.isAdmin) { ?>'+
-                '<a class="delete">Удалить</a>' +
-            '<? } ?> ' +
-        '</div> ' +
-    '<? } ?> ' +
-'</div>' ;
-
+var LIST = '<div class="tab selected" data-id="null">Популярные</div>' +
+'<?if (cur.dataUser.isEditor) { ?>'+
+    '<div class="actions">' +
+        '<a class="share">Поделиться</a>' +
+        '<a class="edit">Редактировать</a>' +
+        '<?if (cur.dataUser.isAdmin) { ?>'+
+            '<a class="delete">Удалить</a>' +
+        '<? } ?> ' +
+    '</div> ' +
+'<? } ?>';
 
 var LIST_ITEM =
 '<? if (isset("itemFave")) { ?>' +
