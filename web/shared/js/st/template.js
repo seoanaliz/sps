cc084a22e134368dcfc7b78c067adb64bda83bf5
Evgeny Kulikov <beyondcompute@gmail.com>
2013-07-18 15:25:33 +0400

    карандашик только для редакторов

--- web/shared/js/st/template.js
+++ web/shared/js/st/template.js
@@ -180,10 +180,10 @@ var FILTER_LIST =
 
 var FILTER_LIST_ITEM =
 '<div class="item" title="<?=name?>" data-id="<?=id?>">' +
-    '<span class="edit icon"></span>' +
     '<span class="text"><?=name?></span>' +
     '<?if (cur.dataUser.isEditor) { ?>'+
-        '<div class="icon bookmark"></div>' +
+        '<span class="icon edit"></span>' +
+        '<span class="icon bookmark"></span>' +
     '<? } ?>'+
 '</div>';
 
