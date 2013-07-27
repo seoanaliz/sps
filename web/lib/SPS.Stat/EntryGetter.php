f7a854101ada91a9eca3a832e5d0e4965e27c22b
Evgeny Kulikov <beyondcompute@gmail.com>
2013-07-27 10:15:14 +0400

    сократил размер незжатого файла

--- web/lib/SPS.Stat/EntryGetter.php
+++ web/lib/SPS.Stat/EntryGetter.php
@@ -111,7 +111,7 @@ class EntryGetter {
                 'vk_id'     =>  $vkPublic->vk_id,
                 'quantity'  =>  $vkPublic->quantity,
                 'name'      =>  $vkPublic->name,
-                'ava'       =>  $vkPublic->ava,
+                'ava'       =>  trim($vkPublic->ava),
                 'group_id'  =>  $groups_ids,
                 'admins'    =>  array(),
                 'diff_abs'  =>  $vkPublic->$diff_abs,
