e6d595c9514d3133a429c6982d0b5a766f037949
Evgeny Kulikov <beyondcompute@gmail.com>
2013-07-27 09:11:40 +0400

    снова выпилил .global-loader

--- web/etc/templates/st/index.tmpl.php
+++ web/etc/templates/st/index.tmpl.php
@@ -7,7 +7,6 @@
 ?>
 
 {increal:tmpl://st/elements/header.tmpl.php}
-<div id="global-loader"></div>
 <div id="main" class="main">
     <div class="header clear-fix">
         <div class="tab-bar"></div>
