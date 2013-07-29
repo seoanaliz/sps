3910eb7d74ac28fd9a2aa4838a636a41ce194867
akalie <askaslie@gmail.com>
2013-07-29 16:24:02 +0400

    добавил выборку всех токенов для targetFeed

--- web/lib/SPS.Articles/AccessTokenUtility.php
+++ web/lib/SPS.Articles/AccessTokenUtility.php
@@ -26,6 +26,33 @@
             return $access_token;
         }
 
+        public static function getAllTokens( $targetFeedId, $version = 0 ) {
+            $result = array();
+
+            $search = array(
+                'targetFeedId'  =>  $targetFeedId,
+                '_role'         =>  array(UserFeed::ROLE_ADMINISTRATOR, UserFeed::ROLE_OWNER, UserFeed::ROLE_EDITOR),
+            );
+
+            $userFeeds = UserFeedFactory::Get($search);
+            if( empty($userFeeds)) {
+                return $result;
+            }
+            $vkIds = ArrayHelper::GetObjectsFieldValues($userFeeds, array('vkId'));
+            $searchAT = array( 'vkIdIn' => array_values($vkIds));
+            if( $version ) {
+                $searchAT['version'] = $version;
+            }
+
+            $access_tokens = AccessTokenFactory::Get($searchAT);
+            foreach( $access_tokens as $access_token ) {
+                if( !empty( $access_token->accessToken)) {
+                    $result[$access_token->vkId] = $access_token->accessToken;
+                }
+            }
+            return $result;
+        }
+
         /** @var $targetFeed TargetFeed*/
         public static function getPublisherTokenForTargetFeed($targetFeedId, $random = false )
         {
