777aef29eb16698bb8638186d10428b78e83453d
akalie <askaslie@gmail.com>
2013-07-30 17:58:31 +0400

    муахаха, починил всею отправку

--- web/shared/js/st/main.js
+++ web/shared/js/st/main.js
@@ -101,7 +101,7 @@ function makeVkButton() {
     if ($loginInfo.length) {
         var vkHref = 'https://oauth.vk.com/authorize?' +
                     'client_id='+ Configs.appId +
-                    '&scope=stats,groups,offline,wall' +
+                    '&scope=stats,groups,offline,wall,photos' +
                     '&redirect_uri='+ encodeURIComponent(location.protocol + '//' + location.host + '/vk-login/?to=' + location.pathname) +
                     '&display=page' +
                     '&response_type=code';
