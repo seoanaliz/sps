22e4a5623859a3819504374f4c0e9bd8afaacd70
Evgeny Reznichenko <kusakyky@gmail.com>
2013-09-17 18:49:42 +0400

    Стили для поля ввода времени лока

--- web/etc/templates/fe/elements/articles-queue-list-item.tmpl.php
+++ web/etc/templates/fe/elements/articles-queue-list-item.tmpl.php
@@ -50,13 +50,11 @@
                 <span class="time-of-removal <?= $deleteAt ? 'visible': '' ?>"></span>
                 <span class="time-of-remove"><?= $deleteAt ? $deleteAt : '' ?></span>
                 {increal:tmpl://fe/elements/articles-queue-item-header.tmpl.php}
-
                 <? if (empty($gridItem['blocked']) && $canEditQueue) { ?>
                     <div class="locked-trigger"></div>
                     <div class="edit-trigger"></div>
                     <div class="delete"></div>
                 <? } ?>
-
             </div>
         <? } ?>
         <div class="post movable
