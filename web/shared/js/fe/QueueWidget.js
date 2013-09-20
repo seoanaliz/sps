3b637ec7ff93eedf0ee1f619dea66c0747b76fcd
Evgeny Reznichenko <kusakyky@gmail.com>
2013-09-20 14:04:17 +0400

    фикс работы инбута замочка

--- web/shared/js/fe/QueueWidget.js
+++ web/shared/js/fe/QueueWidget.js
@@ -333,9 +333,10 @@ var QueueWidget = Event.extend({
                 $input = $('<input />')
                     .attr('type', 'text')
                     .attr('class', 'time-of-locked-edit')
-                    .width($time.width() + 2)
+                    .width($time.width())
                     .mask('29:59')
                     .appendTo($post);
+
                 $time.data('time-of-locked-edit', $input);
             } else {
                 $input.show();
@@ -349,8 +350,11 @@ var QueueWidget = Event.extend({
         $queue.delegate('.time-of-locked-edit', 'blur keydown', function (e) {
             var $input = $(this);
 
-            if (~['keydown', 'focusout'].indexOf(e.type)
-                && (!e.originalEvent || e.keyCode !== KEY.ENTER)) {
+            if (e.type === 'keydown' && e.keyCode != KEY.ENTER) {
+                return;
+            }
+
+            if (e.type === 'focusout' && !e.originalEvent) {
                 return;
             }
 
