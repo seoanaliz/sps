5f395f1ed08f2c60fc941e1261e123060bac39e6
akalie <akalie@users.noreply.github.com>
2013-10-17 12:28:36 +0300

    уменьшил интервал защиты на 30 сек, что бы часовые интервалы не перекрывались

--- web/lib/SPS.Site/actions/controls/PostSetProtectedControl.php
+++ web/lib/SPS.Site/actions/controls/PostSetProtectedControl.php
@@ -43,7 +43,9 @@ class PostSetProtectedControl extends BaseControl {
             list($hour, $minutes) = explode(':', $time);
             $ts = $articleQueue->startDate->getTimestamp();
             $protectTo = new DateTimeWrapper(null);
-            $protectTo->setTimestamp($ts)->modify('+' . $hour . ' hours')->modify('+' . $minutes . ' minutes');
+            $protectTo->setTimestamp($ts)->modify('+' . $hour . ' hours')
+                ->modify('+' . $minutes . ' minutes')
+                ->modify('-30 seconds');
             if ( $hour > 5 || $protectTo <= $articleQueue->startDate ) {
                 $result['message'] = 'Wrong protect time';
                 die(ObjectHelper::ToJSON($result));
